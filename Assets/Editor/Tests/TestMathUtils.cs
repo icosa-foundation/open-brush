icosa-foundation/open-brush

commit 11e835334122ac0f441675a51e2201ec6a28d1be

Add pylintrc, flake8, pre-commit for Python scripts. Fix outstanding warnings / errors.  (#35)
* Add .flake8 and pylintrc, and .pre-commit-config.yaml for enforcement

* Fix code quality warnings (or ignore them)

Mostly silly stuff, but a few important fixes
* replace file() with open() (python 3+)
* Catch a few renames that were missed in the rebranding from tilt_brush
  to open_brush
* python3 style super() and class definitions
* Tweak the keyring import slightly
* There's a possible bug in bvh.py, when iterating and returning leafs.
  See the TODO
* Replace some map()s with list and set comprehensions

* Use utf-8 encoding for handling byte->str in git subprocess

* Add rewritten Support/ThirdParty/GeneratedThirdPartyNotices.txt (different order)

* Also add Support/bin to .pre-commit-config

* Ignore duplicate-code

* Also fix/ignore pylint and flake8 on Support/bin

* Create main.yml

Support/Python/tbdata/brush_lookup.py
@@ -16,18 +16,20 @@ import os
 import re
 from collections import defaultdict
 
-class BrushLookup(object):
+
+class BrushLookup():
   """Helper for doing name <-> guid conversions for brushes."""
   @staticmethod
   def iter_brush_guid_and_name(tilt_brush_dir):
     for brush_dir in ("Assets/Resources/Brushes", "Assets/Resources/X/Brushes"):
-      for r, ds, fs in os.walk(os.path.join(tilt_brush_dir, brush_dir)):
+      for r, _, fs in os.walk(os.path.join(tilt_brush_dir, brush_dir)):
         for f in fs:
           if f.lower().endswith('.asset'):
             fullf = os.path.join(r, f)
-            data = file(fullf).read()
+            with open(fullf) as f:
+              data = f.read()
             guid = re.search('m_storage: (.*)$', data, re.M).group(1)
-            name = re.search('m_Name: (.*)$', data, re.M).group(1)
+            # name = re.search('m_Name: (.*)$', data, re.M).group(1)
 
             yield guid, f[:-6]
 
@@ -48,10 +50,10 @@ class BrushLookup(object):
     self.initialized = True
     self.guid_to_name = dict(self.iter_brush_guid_and_name(tilt_brush_dir))
     # Maps name -> list of guids
-    self.name_to_guids = defaultdict(list)
+    name_to_guids = defaultdict(list)
     for guid, name in self.guid_to_name.items():
-      self.name_to_guids[name].append(guid)
-    self.name_to_guids = dict(self.name_to_guids)
+      name_to_guids[name].append(guid)
+    self.name_to_guids = dict(name_to_guids)
 
   def get_unique_guid(self, name):
     lst = self.name_to_guids[name]

.flake8
@@ -1,0 +1,2 @@
+[flake8]
+extend-ignore = E111, E114, E501, E722, E121

.github/workflows/main.yml
@@ -1,0 +1,14 @@
+name: pre-commit
+
+on:
+  pull_request:
+  push:
+    branches: [main]
+
+jobs:
+  pre-commit:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v2
+    - uses: actions/setup-python@v2
+    - uses: pre-commit/action@v2.0.0

.pre-commit-config.yaml
@@ -1,0 +1,16 @@
+---
+repos:
+  - repo: https://github.com/pre-commit/pre-commit-hooks
+    rev: v2.0.0
+    hooks:
+      - id: flake8
+        files: ^Support/
+  - repo: https://github.com/PyCQA/pylint.git
+    rev: pylint-2.6.0
+    hooks:
+      - id: pylint
+        name: pylint
+        files: ^Support/
+        language_version: python3
+        args:
+          - --load-plugins=pylint.extensions.redefined_variable_type,pylint.extensions.bad_builtin

.pylintrc
@@ -1,0 +1,2 @@
+[MESSAGES CONTROL]
+disable=bad-indentation,missing-class-docstring,missing-module-docstring,missing-function-docstring,invalid-name,fixme,line-too-long,duplicate-code

Support/Python/tbdata/bvh.py
@@ -16,9 +16,9 @@
 # Usage:
 #   RTree.from_bounds_iter()
 
-import os
 import struct
 import sys
+from collections import deque
 from io import StringIO
 
 try:
@@ -37,21 +37,21 @@ class BBox(tuple):
   def union(lhs, rhs):
     lmin, lmax = lhs
     rmin, rmax = rhs
-    nmin = ( min(lmin[0], rmin[0]),
-             min(lmin[1], rmin[1]),
-             min(lmin[2], rmin[2]) )
-    nmax = ( max(lmax[0], rmax[0]),
-             max(lmax[1], rmax[1]),
-             max(lmax[2], rmax[2]) )
+    nmin = (min(lmin[0], rmin[0]),
+            min(lmin[1], rmin[1]),
+            min(lmin[2], rmin[2]))
+    nmax = (max(lmax[0], rmax[0]),
+            max(lmax[1], rmax[1]),
+            max(lmax[2], rmax[2]))
     for i in range(3):
-      assert nmax[i] > nmin[i], self
+      assert nmax[i] > nmin[i]
     return BBox((nmin, nmax))
 
   def half_width(self):
     bmin, bmax = self
-    return ((bmax[0]-bmin[0]) * .5,
-            (bmax[1]-bmin[1]) * .5,
-            (bmax[2]-bmin[2]) * .5)
+    return ((bmax[0] - bmin[0]) * .5,
+            (bmax[1] - bmin[1]) * .5,
+            (bmax[2] - bmin[2]) * .5)
 
   def surface_area(self):
     dx, dy, dz = self.half_width()
@@ -63,21 +63,24 @@ class BBox(tuple):
 # RTree
 # ---------------------------------------------------------------------------
 
+
 # Tunables
 INDEX_CAPACITY = 80
 LEAF_CAPACITY = 410
 
+
 def str_vec3(vec3):
   return "(%6.1f %6.1f %6.1f)" % vec3
 
+
 def str_bounds(bounds):
-  #return "(%s, %s)" % (str_vec3(bounds[0]), str_vec3(bounds[1]))
+  # return "(%s, %s)" % (str_vec3(bounds[0]), str_vec3(bounds[1]))
   bmin, bmax = bounds
-  halfsize = (bmax[0]-bmin[0], bmax[1]-bmin[1], bmax[2]-bmin[2])
+  halfsize = (bmax[0] - bmin[0], bmax[1] - bmin[1], bmax[2] - bmin[2])
   return "(%5.1f %5.1f %5.1f)" % halfsize
 
 
-class BinaryReader(object):
+class BinaryReader():
   # Wraps struct.unpack
   def __init__(self, inf):
     if isinstance(inf, bytes):
@@ -104,9 +107,11 @@ class RTreeStorageDict(rtree.index.CustomStorage):
   def flush(self, returnError):
     # print "flush"
     pass
+
   def create(self, returnError):
     # print "create"
     pass
+
   def destroy(self, returnError):
     # print "destroy"
     pass
@@ -120,25 +125,26 @@ class RTreeStorageDict(rtree.index.CustomStorage):
     except KeyError:
       returnError.contents.value = self.InvalidPageError
 
-  def storeByteArray(self, page, data, returnError):
+  def storeByteArray(self, page, data):
     if page == self.NewPage:
       page = len(self.datas)
       self.datas[page] = data
       # print "store new %s %s" % (page, len(data))
     else:
       assert page in self.datas
-      old_data = self.datas[page]
+      # old_data = self.datas[page]
       self.datas[page] = data
       # print "store %s %s -> %s" % (page, len(old_data), len(data))
 
     return page
 
-  def deleteByteArray(self, page, returnError):
+  def deleteByteArray(self, page):
     # print "RTreeStorageDict: delete", page
     assert page in self.datas
     self.datas[page] = 'deleted'
-    
-class RTree(object):
+
+
+class RTree():
   @classmethod
   def from_bounds_iter(cls, bounds_iter, leaf_capacity_multiplier=1):
     storage = RTreeStorageDict()
@@ -168,11 +174,10 @@ class RTree(object):
       for c in node.children:
         assert c.data is None
         c.node = self._recursive_create_node(storage, c.id)
-      
+
     return node
 
   def dfs_iter(self):
-    from collections import deque
     q = deque([self.root])
     while q:
       n = q.popleft()
@@ -181,7 +186,7 @@ class RTree(object):
         q.extend(c.node for c in n.children)
 
 
-class RTreeHeader(object):
+class RTreeHeader():  # pylint: disable=too-few-public-methods,too-many-instance-attributes
   # RTree::storeHeader
   #  id_type    root
   #  u32        RTreeVariant (0 linear, 1 quadratic, 2 rstar)
@@ -220,7 +225,9 @@ class RTreeHeader(object):
 
 class CannotSplit(Exception):
   pass
-class RTreeNode(object):
+
+
+class RTreeNode():
   # for nodeType
   PERSISTENT_INDEX = 1
   PERSISTENT_LEAF = 2
@@ -240,7 +247,7 @@ class RTreeNode(object):
     self.node_id = node_id
     reader = BinaryReader(inf_or_data)
     (self.nodeType, self.level, nChildren) = reader.read("III")
-    self.children = [ RTreeChild(reader) for i in range(nChildren) ]
+    self.children = [RTreeChild(reader) for i in range(nChildren)]
     self.bounds = reader.read_bounds()
 
   def is_index(self):
@@ -267,6 +274,7 @@ class RTreeNode(object):
     #   isinstance(v, RTreeChild) == True
     #   v.node.is_leaf() == True
     bounds = []
+
     def get_all_bounds(node, bounds):
       if node.is_leaf():
         for c in node.children:
@@ -279,6 +287,7 @@ class RTreeNode(object):
     new_node = RTree.from_bounds_iter(bounds, multiplier).root
     if new_node.is_leaf():
       raise CannotSplit()
+
     def iter_leaf_children(node):
       for c in node.children:
         if c.node.is_leaf():
@@ -286,7 +295,8 @@ class RTreeNode(object):
           RTreeNode.GENERATED_SPLIT_ID -= 1
           yield c
         else:
-          for leaf in iter_leaf_children(c.node):
+          for leaf in iter_leaf_children(c.node):  # pylint: disable=unused-variable
+            # TODO: Should this be "yield leaf"? (if so, remove the pylint comment above)
             yield c
 
     lst = list(iter_leaf_children(new_node))
@@ -301,7 +311,7 @@ class RTreeNode(object):
       str_bounds(self.bounds))
 
 
-class RTreeChild(object):
+class RTreeChild():  # pylint: disable=too-few-public-methods
   # .id
   #   If parent is PERSISTENT_INDEX, this is a node id.
   #   If parent is PESISTENT_LEAF, this is some leaf-specific id (like a stroke number)

Support/Python/tbdata/glb.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 
 
-
 import itertools
 import json
 import os
@@ -46,7 +45,7 @@ def grouper(n, iterable, fillvalue=None):
   return itertools.zip_longest(fillvalue=fillvalue, *args)
 
 
-class binfile(object):
+class binfile():
   # Helper for parsing
   def __init__(self, inf):
     self.inf = inf
@@ -78,13 +77,13 @@ class binfile(object):
     return self.inf.write(data)
 
 
-class BaseGltf(object):
+class BaseGltf():
   """Abstract subclass for classes that parse:
   - gltf+bin
   - glb version 1
   - glb version 2"""
   # Jeez
-  PLURAL_SUFFIX = { 'mesh': 'es' }
+  PLURAL_SUFFIX = {'mesh': 'es'}
 
   @staticmethod
   def create(filename):
@@ -93,17 +92,22 @@ class BaseGltf(object):
     first_bytes = bf.read(4)
     if first_bytes == 'glTF':
       version, = bf.unpack("I")
-      if version == 1: return Glb1(filename)
-      elif version == 2: return Glb2(filename)
+      if version == 1:
+        return Glb1(filename)
+      if version == 2:
+        return Glb2(filename)
       raise Exception("Bad version %d" % version)
-    elif filename.lower().endswith('.gltf') or first_bytes.startswith("{"):
+    if filename.lower().endswith('.gltf') or first_bytes.startswith("{"):
       return Gltf(filename)
-    else:
-      raise Exception("Unknown format")
+    raise Exception("Unknown format")
 
   def __init__(self, filename):
     self.filename = filename
     # subclass will init version, json_chunk, json, and bin_chunk
+    self.json = None
+    self.version = None
+    self.bin_chunk = None
+    self.json_chunk = None
 
   def dereference(self):
     """Converts (some) inter-object references from ints/strings to
@@ -113,13 +117,16 @@ class BaseGltf(object):
       # Deref obj[prop]
       dest_type = dest_type or prop  # prop name is usually the obj type
       lookup_table_name = dest_type + 's'
-      try: idx_or_name = obj[prop]
-      except KeyError: pass
-      else: obj[prop+'_'] = self.json[lookup_table_name][idx_or_name]
+      try:
+        idx_or_name = obj[prop]
+      except KeyError:
+        pass
+      else:
+        obj[prop + '_'] = self.json[lookup_table_name][idx_or_name]
 
     def deref_all(source_type, prop, dest_type=None):
       # Deref obj[prop] for all objs of type source_type
-      for name_or_idx, obj in self.iter_objs(source_type):
+      for _, obj in self.iter_objs(source_type):
         deref_property(obj, prop, dest_type)
 
     deref_all('accessor', 'bufferView')
@@ -138,23 +145,22 @@ class BaseGltf(object):
     if self.version == 1:
       plural = self.PLURAL_SUFFIX.get(obj_type, 's')
       return list(self.json[obj_type + plural].items())
-    elif self.version == 2:
+    if self.version == 2:
       plural = self.PLURAL_SUFFIX.get(obj_type, 's')
       return enumerate(self.json[obj_type + plural])
-    else:
-      raise Exception("Unknown gltf version; cannot iterate objects")
-
+    raise Exception("Unknown gltf version; cannot iterate objects")
 
   # backwards-compat
-  def get_json(self): return self.json_chunk
+  def get_json(self):
+    return self.json_chunk
 
   def get_mesh_by_name(self, name):
     if self.version == 1:
       return self.json['meshes'][name]
-    else:
-      for m in self.json['meshes']:
-        if m['name'] == name: return m
-      raise LookupError(name)
+    for m in self.json['meshes']:
+      if m['name'] == name:
+        return m
+    raise LookupError(name)
 
   def get_bufferView_data(self, buffer_view):
     """Returns a hunk of bytes."""
@@ -172,16 +178,17 @@ class BaseGltf(object):
     flat_count = accessor['count'] * count_per_element
     byte_length = flat_count * SIZES[componentType]
     bufferview_data = self.get_bufferView_data(accessor['bufferView_'])
-    attr_data = bufferview_data[start : start + byte_length]
+    attr_data = bufferview_data[start: start + byte_length]
     struct_format = '<' + str(flat_count) + STRUCT_FORMAT[componentType]
     flat = struct.unpack(struct_format, attr_data)
-    if count_per_element == 1: return flat
-    else: return list(grouper(count_per_element, flat))
+    if count_per_element == 1:
+      return flat
+    return list(grouper(count_per_element, flat))
 
 
 class Gltf(BaseGltf):
   def __init__(self, filename):
-    super(Gltf, self).__init__(filename)
+    super().__init__(filename)
     # Not fully general; just good enough to work for TB .gltf/bin pairs
     bin_name = os.path.splitext(filename)[0] + '.bin'
     if not os.path.exists(bin_name):
@@ -196,7 +203,7 @@ class Gltf(BaseGltf):
 
 class Glb1(BaseGltf):
   def __init__(self, filename):
-    super(Glb1, self).__init__(filename)
+    super().__init__(filename)
     bf = binfile(open(self.filename, 'rb'))
     assert bf.read(4) == 'glTF'
     self.version, self.total_len, json_len, json_fmt = bf.unpack("<4I")
@@ -208,6 +215,7 @@ class Glb1(BaseGltf):
 
 class Glb2(BaseGltf):
   def __init__(self, filename):
+    super().__init__()
     self.filename = filename
     bf = binfile(open(self.filename, 'rb'))
     assert bf.read(4) == 'glTF'
@@ -218,7 +226,8 @@ class Glb2(BaseGltf):
     self.bin_chunk = self._read_chunk(bf, 'BIN\0')
     self.json = json.loads(self.json_chunk)
 
-  def _read_chunk(self, bf, expect_tag):
+  @staticmethod
+  def _read_chunk(bf, expect_tag):
     length, = bf.unpack("I")
     tag = bf.read(4)
     assert tag == expect_tag, tag
@@ -230,6 +239,8 @@ class Glb2(BaseGltf):
 # Testing
 #
 
+# pylint: disable=all
+# flake8: noqa
 def load(version, name):
   ROOT = os.path.expanduser('~/Documents/Tilt Brush/Exports/Baseline 22.0b4')
   formatname = 'glb1' if (version == 1) else 'glb'

commit aa0d07395879f7b92a2bbf6b6895c170858acd1e

Fix #77 and #79 (#78)
Fix #77 
Fix #79

* ignore .vsconfig

This is something to do with visual studio tools for unity

Change-Id: Ie3284f485c4e41879448612ec67058784fa75cc2

* BrushLookup: iter_brush_guid_and_name

Broken by 11e8353; local variable f was being stomped

Change-Id: Ibd53c091f101843fc12e57a096335bec66460df3

* BrushLookup: also report which brushes are standard

Change-Id: Ic17285516ec336bb1b4eac87217922154f7ed647

* Address #77

There was another edge case where LinearResample would try to read
one past the end of the array.  Fixed.

Added test for that edge case

Change-Id: I3c7c09a7bde93aceff8816874546c069b3ba99b3

* Address #79

Brush descriptor is null for unknown brushes

Change-Id: I898e84393b07eda2dacd01890cf187667734fc36

.gitignore
@@ -26,6 +26,7 @@
 *.suo
 /*.sln
 /*.userprefs
+.vsconfig
 .vs
 .vscode/
 

Assets/Scripts/Playback/ScenePlaybackByDistance.cs
@@ -31,7 +31,7 @@ public class StrokePlaybackByDistance : StrokePlayback {
     m_lastPosition = m_stroke.m_ControlPoints[0].m_Pos;
 
     var desc = BrushCatalog.m_Instance.GetBrush(stroke.m_BrushGuid);
-    m_playBackAtStrokeGranularity = (m_parent.QuickLoad || desc.m_PlayBackAtStrokeGranularity);
+    m_playBackAtStrokeGranularity = (m_parent.QuickLoad || (desc?.m_PlayBackAtStrokeGranularity ?? true));
   }
 
   protected override bool IsControlPointReady(PointerManager.ControlPoint controlPoint) {

Assets/Scripts/Util/MathUtils.cs
@@ -886,17 +886,18 @@ static public class MathUtils {
     // The fractional portion will be used for a lerp.
     double oldFromNew = (samples.Length - 1) / ((double)newSamples - 1);
 
-    // The very last sample will try to do a Lerp(oldSamples[count-1], oldSamples[count], 0)
-    // which reads off the end of the array (but throws away the value in the lerp).
-    // Hack around it by treating that last sample specially.
-    for (int idxNew = 0; idxNew < newSamples-1; ++idxNew) {
+    for (int idxNew = 0; idxNew < newSamples; ++idxNew) {
       double idxOldf = oldFromNew * idxNew;   // In general, this will be between two samples
       double idxOldFloor = Math.Floor(idxOldf);
       int idxOld = (int)idxOldFloor;
+      // We run off the end of the array in a couple cases:
+      // - the last sample, which tries to Lerp(oldSamples[count-1], oldSamples[count], 0)
+      // - samples.Length == 1
+      // In both cases, t==0 so it doesn't matter what index we use, so this clamp is fine
+      int idxOldPlus = Mathf.Min(idxOld+1, (int)samples.Length-1);
       double t = idxOldf - idxOldFloor;
-      yield return Mathf.LerpUnclamped(samples[idxOld], samples[idxOld+1], (float)t);
+      yield return Mathf.LerpUnclamped(samples[idxOld], samples[idxOldPlus], (float)t);
     }
-    yield return samples[samples.Length-1];
   }
 
 }  // MathUtils

Support/Python/tbdata/brush_lookup.py
@@ -11,51 +11,67 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# mypy: no-strict-optional
 
 import os
 import re
+from typing import cast, Dict, Iterator, List, Tuple, NewType
 from collections import defaultdict
+
+Guid = NewType('Guid', str)
 
 
 class BrushLookup():
   """Helper for doing name <-> guid conversions for brushes."""
+
+  def iter_standard_brush_guids(self) -> Iterator[Guid]:
+    """Yields all the standard (non-experimental) brush guids"""
+    with open(os.path.join(self.dir, 'Assets/Manifest.asset')) as inf:
+      data = inf.read().replace('\r', '')
+    brush_chunk = re.search(r'Brushes:\n(  -.*\n)*', data).group(0)
+    for match in re.finditer(r'guid: ([0-9a-f]{32})', brush_chunk):
+      yield cast(Guid, match.group(1))
+
   @staticmethod
-  def iter_brush_guid_and_name(tilt_brush_dir):
+  def iter_brush_guid_and_name(tilt_brush_dir: str) -> Iterator[Tuple[Guid, str]]:
+    """Yields (guid, name) tuples."""
     for brush_dir in ("Assets/Resources/Brushes", "Assets/Resources/X/Brushes"):
       for r, _, fs in os.walk(os.path.join(tilt_brush_dir, brush_dir)):
         for f in fs:
           if f.lower().endswith('.asset'):
             fullf = os.path.join(r, f)
-            with open(fullf) as f:
-              data = f.read()
-            guid = re.search('m_storage: (.*)$', data, re.M).group(1)
+            with open(fullf) as inf:
+              data = inf.read()
+            guid = cast(Guid, re.search('m_storage: (.*)$', data, re.M).group(1))
             # name = re.search('m_Name: (.*)$', data, re.M).group(1)
+            name = f[:-6]
+            yield guid, name
 
-            yield guid, f[:-6]
-
-  _instances = {}
+  _instances: Dict[str, 'BrushLookup'] = {}
 
   @classmethod
-  def get(cls, tilt_brush_dir=None):
+  def get(cls, tilt_brush_dir=None) -> 'BrushLookup':
     if tilt_brush_dir is None:
       tilt_brush_dir = os.path.normpath(os.path.join(os.path.abspath(__file__), "../../../.."))
 
     try:
       return cls._instances[tilt_brush_dir]
     except KeyError:
-      val = cls._instances[tilt_brush_dir] = BrushLookup(tilt_brush_dir)
-      return val
+      pass
+    val = cls._instances[tilt_brush_dir] = BrushLookup(tilt_brush_dir)
+    return val
 
-  def __init__(self, tilt_brush_dir):
+  def __init__(self, tilt_brush_dir: str):
+    self.dir = tilt_brush_dir
     self.initialized = True
-    self.guid_to_name = dict(self.iter_brush_guid_and_name(tilt_brush_dir))
-    # Maps name -> list of guids
-    name_to_guids = defaultdict(list)
+    self.guid_to_name = dict(self.iter_brush_guid_and_name(self.dir))
+    self.standard_brushes = set(self.iter_standard_brush_guids())
+    name_to_guids: Dict[str, List[Guid]] = defaultdict(list)
     for guid, name in self.guid_to_name.items():
       name_to_guids[name].append(guid)
     self.name_to_guids = dict(name_to_guids)
 
-  def get_unique_guid(self, name):
+  def get_unique_guid(self, name: str) -> Guid:
     lst = self.name_to_guids[name]
     if len(lst) == 1:
       return lst[0]

Assets/Editor/Tests/TestMathUtils.cs
@@ -810,5 +810,15 @@ internal class TestMathUtils : MathTestUtils {
     Assert.AreEqual(floats[0], floats1000[0]);
     Assert.AreEqual(floats[floats.Length-1], floats1000[floats1000.Length-1]);
   }
+
+  [Test]
+  public void TestLinearResampleOne() {
+    // https://github.com/icosa-gallery/open-brush/issues/77
+    // LinearResample was breaking on arrays of length 1
+    float[] floats1 = new float[] { 3.5f };
+    float[] floats4 = MathUtils.LinearResampleCurve(floats1, 4).ToArray();
+    Assert.AreEqual(floats1[0], floats4[0]);
+    Assert.AreEqual(floats1[0], floats4[3]);
+  }
 }
 }