6010ea90962ae62056b9636b07b186b5cf6080d0
Andy Baker <andy@andybak.net>
2021-09-29 13:08:06 +0100

    Better Snap settings UI
    [CI BUILD]

--- Assets/Scripts/GUI/SnapSettingsButton.cs
+++ Assets/Scripts/GUI/SnapSettingsButton.cs
@@ -14,14 +14,6 @@ namespace TiltBrush
 
         public SettingTypes SettingType;
 
-        protected override float OptionAngleDeltaDegrees
-        {
-            get
-            {
-                return 120; // Button looks weird if the angle is too shallow so fake it
-            }
-        }
-
         protected override void OnStart()
         {
             CreateOptionSides();
