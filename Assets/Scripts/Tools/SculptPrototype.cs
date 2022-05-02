d4fe6f8563bf833e5c026a35cee2f7296bf31dff
cdadashovkhandan <chingizdkhandan@gmail.com>
2022-05-02 12:17:17 +0200

    fix repositioning bug

--- Assets/Scripts/Tools/SculptPrototype.cs
+++ Assets/Scripts/Tools/SculptPrototype.cs
@@ -33,6 +33,7 @@ namespace TiltBrush
 
             // Call this after setting up our tool's state.
             base.EnableTool(bEnable);
+            //change the material of all tools to some wireframe shader.
             HideTool(!bEnable);
         }
 
@@ -43,11 +44,12 @@ namespace TiltBrush
             int lastIdx = firstIdx + rGroup.m_VertLength;
             
             var newVertices =  parentBatch.m_MeshFilter.mesh.vertices;
+            var toolPos = m_CurrentCanvas.Pose.inverse * m_ToolTransform.position;
             for (int i = firstIdx; i < lastIdx; i++) {
-                if (Vector3.Distance(newVertices[i], m_ToolTransform.position) < 0.5) // close enough to pointer
+                if (Vector3.Distance(newVertices[i], toolPos) < 0.5) // close enough to pointer
                 {
                     // CTODO: the math is scuffed.
-                    Vector3 direction = (newVertices[i] - m_ToolTransform.position).normalized;
+                    Vector3 direction = (newVertices[i] - toolPos).normalized;
                     Vector3 newVert = newVertices[i] + direction * 0.2f; 
                     newVertices[i] = newVert;
                 }
