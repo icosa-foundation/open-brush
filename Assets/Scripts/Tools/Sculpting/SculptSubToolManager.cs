ca3a958e4f17d277116d0d7ffdd15a28405e4fdb
cdadashovkhandan <chingizdkhandan@gmail.com>
2022-07-07 12:26:02 +0200

    add button toggle higlight and missing copyright notice

--- Assets/Scripts/Tools/Sculpting/SculptSubToolManager.cs
+++ Assets/Scripts/Tools/Sculpting/SculptSubToolManager.cs
@@ -1,4 +1,18 @@
-﻿using System.Collections;
+﻿// Copyright 2022 Chingiz Dadashov-Khandan
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -21,6 +35,8 @@ public class SculptSubToolManager : MonoBehaviour {
     Rotate,
   }
 
+  private SubTool m_ActiveSubtool = SubTool.Push;
+
   void Awake() {
     m_Instance = this;
     m_SubTools = new List<BaseSculptSubTool>();
@@ -28,8 +44,13 @@ public class SculptSubToolManager : MonoBehaviour {
         m_SubTools.Add(child.gameObject.GetComponent<BaseSculptSubTool>());
     }
   }
+
+  public SubTool GetActiveSubtool() {
+    return m_ActiveSubtool;
+  }
   
   public void SetSubTool(SubTool subTool) {
+    m_ActiveSubtool = subTool;
     m_SculptTool.SetSubTool(m_SubTools[(int) subTool]);
   }
 }
