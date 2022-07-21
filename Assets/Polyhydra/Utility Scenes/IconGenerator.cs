e39f6d6d3a6f09abef4b3d8a3a99a3928c807f4f
Andy Baker <andy@andybak.net>
2022-07-21 19:56:56 +0100

    Updates to the icon generator

--- Assets/Polyhydra/Utility Scenes/IconGenerator.cs
+++ Assets/Polyhydra/Utility Scenes/IconGenerator.cs
@@ -4,11 +4,13 @@ using Polyhydra.Wythoff;
 using TiltBrush;
 using TiltBrush.MeshEditing;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 
 [ExecuteInEditMode]
 public class IconGenerator : MonoBehaviour
 {
+    public Color[] DefaultColors;
     public Vector3 cameraPosition;
     public int resWidth = 500;
     public int resHeight = 500;
@@ -16,8 +18,8 @@ public class IconGenerator : MonoBehaviour
 
     private string filename;
     private Camera camera;
-    private PreviewPolyhedron previewPoly;
     private bool takeShot;
+    public bool RandomRotation;
 
 
     void Init()
@@ -25,9 +27,8 @@ public class IconGenerator : MonoBehaviour
         camera = Camera.main;
         camera.clearFlags = CameraClearFlags.Nothing;
         camera.backgroundColor = Color.white;
-        previewPoly = GetComponentInChildren<PreviewPolyhedron>(true);
-        previewPoly.Init();
-        previewPoly.gameObject.SetActive(true);
+        PreviewPolyhedron.m_Instance.Init();
+        PreviewPolyhedron.m_Instance.gameObject.SetActive(true);
     }
 
     [ContextMenu("Take Screenshot")]
@@ -41,7 +42,57 @@ public class IconGenerator : MonoBehaviour
     public void BuildNow()
     {
         Init();
-        previewPoly.ImmediateMakePolyhedron();
+        PreviewPolyhedron.m_Instance.ImmediateMakePolyhedron();
+    }
+
+    [ContextMenu("Take All Screenshots")]
+    void TakeAllScreenshots()
+    {
+        TakeAllGridScreenshots();
+        TakeAllRadialScreenshots();
+        TakeAllUniformScreenshots();
+        TakeAllVariousScreenshots();
+        TakeAllShapeScreenshots();
+        TakeCategoryScreenshot();
+    }
+
+    [ContextMenu("Take Waterman Screenshot")]
+    void TakeCategoryScreenshot()
+    {
+        Init();
+        EditableModelManager.CurrentModel.GeneratorType = GeneratorTypes.Waterman;
+        filename = PolyScreenShotName($"Waterman");
+        PreviewPolyhedron.m_Instance.Param1Int = 35;
+        PreviewPolyhedron.m_Instance.Param2Int = 5;
+        CreateThumbnail();
+    }
+
+    [ContextMenu("Take All Shape Screenshots")]
+    void TakeAllShapeScreenshots()
+    {
+        Init();
+        var names = Enum.GetNames(typeof(ShapeTypes));
+        EditableModelManager.CurrentModel.GeneratorType = GeneratorTypes.Shapes;
+
+        for (var index = 0; index < names.Length; index++)
+        {
+            filename = PolyScreenShotName($"other_{names[index]}");
+            PreviewPolyhedron.m_Instance.ShapeType = (ShapeTypes)index;
+            switch (PreviewPolyhedron.m_Instance.ShapeType)
+            {
+                case ShapeTypes.Polygon:
+                case ShapeTypes.Star:
+                    PreviewPolyhedron.m_Instance.Param1Int = 6;
+                    PreviewPolyhedron.m_Instance.Param2Float = .5f;
+                    break;
+                default:
+                    PreviewPolyhedron.m_Instance.Param1Float = 1f;
+                    PreviewPolyhedron.m_Instance.Param2Float = 1f;
+                    PreviewPolyhedron.m_Instance.Param3Float = 1f;
+                    break;
+            }
+            CreateThumbnail();
+        }
     }
 
     [ContextMenu("Take All Various Poly Screenshots")]
@@ -54,35 +105,69 @@ public class IconGenerator : MonoBehaviour
         for (var index = 0; index < names.Length; index++)
         {
             filename = PolyScreenShotName($"other_{names[index]}");
-            previewPoly.VariousSolidsType = (VariousSolidTypes)index;
-            switch (previewPoly.VariousSolidsType)
+            PreviewPolyhedron.m_Instance.VariousSolidsType = (VariousSolidTypes)index;
+            switch (PreviewPolyhedron.m_Instance.VariousSolidsType)
             {
                 case VariousSolidTypes.Box:
-                    previewPoly.Param1Int = 3;
-                    previewPoly.Param1Int = 3;
-                    previewPoly.Param3Int = 3;
+                    PreviewPolyhedron.m_Instance.Param1Int = 3;
+                    PreviewPolyhedron.m_Instance.Param2Int = 4;
+                    PreviewPolyhedron.m_Instance.Param3Int = 3;
                     break;
                 case VariousSolidTypes.Stairs:
-                    previewPoly.Param1Int = 3;
-                    previewPoly.Param2Int = 3;
-                    previewPoly.Param3Int = 3;
+                    PreviewPolyhedron.m_Instance.Param1Int = 3;
+                    PreviewPolyhedron.m_Instance.Param2Float = 3;
+                    PreviewPolyhedron.m_Instance.Param3Float = 3;
                     break;
                 case VariousSolidTypes.UvHemisphere:
-                    previewPoly.Param1Int = 8;
-                    previewPoly.Param2Int = 8;
+                    PreviewPolyhedron.m_Instance.Param1Int = 8;
+                    PreviewPolyhedron.m_Instance.Param2Int = 8;
                     break;
                 case VariousSolidTypes.UvSphere:
-                    previewPoly.Param1Int = 8;
-                    previewPoly.Param2Int = 8;
+                    PreviewPolyhedron.m_Instance.Param1Int = 8;
+                    PreviewPolyhedron.m_Instance.Param2Int = 8;
                     break;
                 case VariousSolidTypes.Torus:
-                    previewPoly.Param1Int = 8;
-                    previewPoly.Param2Int = 8;
+                    PreviewPolyhedron.m_Instance.Param1Int = 8;
+                    PreviewPolyhedron.m_Instance.Param2Int = 8;
+                    PreviewPolyhedron.m_Instance.Param3Float = 16;
                     break;
             }
             CreateThumbnail();
         }
     }
+
+    [ContextMenu("Take All Grid Shape Screenshots")]
+    void TakeAllGridShapeScreenshots()
+    {
+        Init();
+        EditableModelManager.CurrentModel.GeneratorType = GeneratorTypes.Grid;
+        var gridNames = Enum.GetNames(typeof(GridEnums.GridShapes));
+        PreviewPolyhedron.m_Instance.GridType = GridEnums.GridTypes.K_3_3_3_3_3_3;
+        for (var index = 0; index < gridNames.Length; index++)
+        {
+            switch ((GridEnums.GridShapes)index)
+            {
+                case GridEnums.GridShapes.Sphere:
+                    PreviewPolyhedron.m_Instance.Param1Int = 12;
+                    PreviewPolyhedron.m_Instance.Param2Int = 12;
+                    break;
+                case GridEnums.GridShapes.Plane:
+                    PreviewPolyhedron.m_Instance.Param1Int = 4;
+                    PreviewPolyhedron.m_Instance.Param2Int = 4;
+                    break;
+                case GridEnums.GridShapes.Cone:
+                case GridEnums.GridShapes.Cylinder:
+                case GridEnums.GridShapes.Polar:
+                    PreviewPolyhedron.m_Instance.Param1Int = 8;
+                    PreviewPolyhedron.m_Instance.Param2Int = 8;
+                    break;
+            }
+            filename = PolyScreenShotName($"gridshape_{gridNames[index]}");
+            PreviewPolyhedron.m_Instance.GridShape = (GridEnums.GridShapes)index;
+            CreateThumbnail();
+        }
+    }
+
 
     [ContextMenu("Take All Grid Screenshots")]
     void TakeAllGridScreenshots()
@@ -91,13 +176,13 @@ public class IconGenerator : MonoBehaviour
         Init();
         EditableModelManager.CurrentModel.GeneratorType = GeneratorTypes.Grid;
         var gridNames = Enum.GetNames(typeof(GridEnums.GridTypes));
-        previewPoly.GridShape = GridEnums.GridShapes.Plane;
-        previewPoly.Param1Int = 2;
-        previewPoly.Param2Int = 2;
+        PreviewPolyhedron.m_Instance.GridShape = GridEnums.GridShapes.Plane;
+        PreviewPolyhedron.m_Instance.Param1Int = 2;
+        PreviewPolyhedron.m_Instance.Param2Int = 2;
         for (var index = 0; index < gridNames.Length; index++)
         {
             filename = PolyScreenShotName($"grid_{gridNames[index]}");
-            previewPoly.GridType = (GridEnums.GridTypes)index;
+            PreviewPolyhedron.m_Instance.GridType = (GridEnums.GridTypes)index;
             cameraPosition = new Vector3(0, 1, -.5f);
             CreateThumbnail();
         }
@@ -110,45 +195,81 @@ public class IconGenerator : MonoBehaviour
         Init();
         var radialNames = Enum.GetNames(typeof(RadialSolids.RadialPolyType));
         EditableModelManager.CurrentModel.GeneratorType = GeneratorTypes.Radial;
-        previewPoly.Param1Int = 5;
-        previewPoly.Param2Int = 2;
-        previewPoly.Param1Float = .75f;
-        previewPoly.Param2Float = .75f;
-        previewPoly.Param3Float = .75f;
+        PreviewPolyhedron.m_Instance.Param1Int = 5;
+        PreviewPolyhedron.m_Instance.Param2Int = 2;
+        PreviewPolyhedron.m_Instance.Param1Float = .75f;
+        PreviewPolyhedron.m_Instance.Param2Float = .75f;
+        PreviewPolyhedron.m_Instance.Param3Float = .75f;
         for (var index = 0; index < radialNames.Length; index++)
         {
             filename = PolyScreenShotName($"radial_{radialNames[index]}");
-            previewPoly.RadialPolyType = (RadialSolids.RadialPolyType)index;
+            PreviewPolyhedron.m_Instance.RadialPolyType = (RadialSolids.RadialPolyType)index;
             CreateThumbnail();
         }
     }
 
-    [ContextMenu("Take All Wythoff Screenshots")]
-    void TakeAllWythoffScreenshots()
+    [ContextMenu("Take All Uniform Screenshots")]
+    void TakeAllUniformScreenshots()
     {
         Init();
         var uniformNames = Enum.GetNames(typeof(UniformTypes));
         EditableModelManager.CurrentModel.GeneratorType = GeneratorTypes.Uniform;
-        for (var index = 6; index < uniformNames.Length; index++)
+        for (var index = 5; index < uniformNames.Length; index++)
         {
             filename = PolyScreenShotName($"uniform_{uniformNames[index]}");
-            previewPoly.UniformPolyType = (UniformTypes)index;
+            PreviewPolyhedron.m_Instance.UniformPolyType = (UniformTypes)index;
             CreateThumbnail();
         }
     }
 
     private void CreateThumbnail()
     {
-        previewPoly.ImmediateMakePolyhedron();
-        previewPoly.m_PolyMesh.Recenter();
+        EditableModelManager.CurrentModel.Colors = DefaultColors;
+        EditableModelManager.CurrentModel.ColorMethod = ColorMethods.ByRole;
+        PreviewPolyhedron.m_Instance.ImmediateMakePolyhedron();
+        PreviewPolyhedron.m_Instance.m_PolyMesh.Recenter();
+        switch (EditableModelManager.CurrentModel.GeneratorType)
+        {
+            case GeneratorTypes.Various:
+                PreviewPolyhedron.m_Instance.transform.localScale = Vector3.one;
+                switch (PreviewPolyhedron.m_Instance.VariousSolidsType)
+                {
+                    case VariousSolidTypes.Box:
+                        PreviewPolyhedron.m_Instance.transform.localRotation = Quaternion.Euler(-30, 30, 0);
+                        break;
+                    case VariousSolidTypes.Stairs:
+                        PreviewPolyhedron.m_Instance.transform.localRotation = Quaternion.Euler(0, 90, 0);
+                        break;
+                    default:
+                        PreviewPolyhedron.m_Instance.transform.localRotation = Quaternion.identity;
+                        break;
+                }
+                break;
+            case GeneratorTypes.Uniform:
+                EditableModelManager.CurrentModel.ColorMethod = ColorMethods.BySides;
+                if (PreviewPolyhedron.m_Instance.UniformPolyType == UniformTypes.Cube)
+                {
+                    PreviewPolyhedron.m_Instance.transform.localRotation = Quaternion.Euler(-30, 30, 0);
+                    PreviewPolyhedron.m_Instance.transform.localScale = Vector3.one * .3333f;
+                }
+                else
+                {
+                    PreviewPolyhedron.m_Instance.transform.localRotation = Quaternion.identity;
+                    PreviewPolyhedron.m_Instance.transform.localScale = Vector3.one * .5f;
+                }
+                break;
+            default:
+                PreviewPolyhedron.m_Instance.transform.localScale = Vector3.one;
+                break;
+        }
         camera.transform.position = cameraPosition;
-        PolyhydraPanel.FocusCameraOnGameObject(camera, previewPoly.gameObject, ZoomFactor, false);
+        PolyhydraPanel.FocusCameraOnGameObject(camera, PreviewPolyhedron.m_Instance.gameObject, ZoomFactor, false);
         TakeShotNow();
     }
 
     public static string PolyScreenShotName(string polyName)
     {
-        return string.Format("{0}/poly_{1}.jpg",
+        return string.Format("{0}/{1}.jpg",
             Application.persistentDataPath,
             polyName
         );
@@ -172,6 +293,10 @@ public class IconGenerator : MonoBehaviour
 
     private void TakeShotNow()
     {
+        if (RandomRotation)
+        {
+            PreviewPolyhedron.m_Instance.transform.localRotation = Random.rotation;
+        }
         RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
         camera.targetTexture = rt;
         Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
