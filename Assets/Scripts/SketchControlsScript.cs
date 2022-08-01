5e82e614203443eaa1ef7f14a426516266bc1f02
Andy Baker <andy@andybak.net>
2022-08-01 19:29:15 +0100

    Fix save not being active after creating an editable model
    [CI BUILD]

--- Assets/Scripts/SketchControlsScript.cs
+++ Assets/Scripts/SketchControlsScript.cs
@@ -5169,6 +5169,7 @@ namespace TiltBrush
                 SceneSettings.m_Instance.EnvironmentChanged ||
                 LightsControlScript.m_Instance.LightsChanged ||
                 m_WidgetManager.ModelWidgets.Any(w => w.gameObject.activeSelf) ||
+                m_WidgetManager.EditableModelWidgets.Any(w => w.gameObject.activeSelf) ||
                 m_WidgetManager.StencilWidgets.Any(w => w.gameObject.activeSelf) ||
                 m_WidgetManager.ImageWidgets.Any(w => w.gameObject.activeSelf) ||
                 m_WidgetManager.VideoWidgets.Any(w => w.gameObject.activeSelf) ||
