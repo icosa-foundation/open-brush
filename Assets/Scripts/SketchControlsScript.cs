20179ae14cb13c7de0a8ccffa1d0a0c9d44057bc
Andy Baker <andy@andybak.net>
2022-11-07 18:36:53 +0000

    Add a popup for repaint options on the "Repaint Selected" button itself

--- Assets/Scripts/SketchControlsScript.cs
+++ Assets/Scripts/SketchControlsScript.cs
@@ -148,7 +148,8 @@ namespace TiltBrush
             SymmetryTwoHanded = 6003,
             OpenColorOptionsPopup = 7000,
             ChangeSnapAngle = 8000,
-            MergeBrushStrokes = 10000
+            MergeBrushStrokes = 10000,
+            RepaintOptions = 11500,
         }
 
         public enum ControlsType
@@ -4896,6 +4897,7 @@ namespace TiltBrush
                     // TODO refactor code above to use this method
                     OpenUrl($"http://localhost:{App.HttpServer.HttpPort}/examplescripts");
                     break;
+                case GlobalCommands.RepaintOptions: break; // Intentionally blank.
                 case GlobalCommands.Null: break; // Intentionally blank.
                 default:
                     Debug.LogError($"Unrecognized command {rEnum}");
