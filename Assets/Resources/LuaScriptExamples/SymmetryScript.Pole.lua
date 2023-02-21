30f787990e7d2268830675d161fa719f8b225058
Andy Baker <andy@andybak.net>
2023-02-21 10:45:33 +0000

    Fix "Pole" script

--- Assets/Resources/LuaScriptExamples/SymmetryScript.Pole.lua
+++ Assets/Resources/LuaScriptExamples/SymmetryScript.Pole.lua
@@ -3,21 +3,19 @@ Widgets = {copies={label="Copies", type="int", min=1, max=32, default=6}}
 
 function Start()
     Colors = {
-        {1, 0 , 0},
-        {0, 1 , 0},
+        {1, 0, 0},
+        {0, 1, 0},
     }
 end
 
 function Main()
     rot = {0, 0, 0}
     transforms = {}
-    brush = brush.position
-    wand = wand.position
     for i = 0.0, copies do
         pos = {
-            Mathf.Lerp(brush.x, wand.x, i/copies) - brush.x,
-            Mathf.Lerp(brush.y, wand.y, i/copies) - brush.y,
-            Mathf.Lerp(brush.z, wand.z, i/copies) - brush.z,
+            Mathf.Lerp(brush.position.x, wand.position.x, i/copies) - brush.position.x,
+            Mathf.Lerp(brush.position.y, wand.position.y, i/copies) - brush.position.y,
+            Mathf.Lerp(brush.position.z, wand.position.z, i/copies) - brush.position.z,
         }
         table.insert(transforms, {pos, rot})
     end
