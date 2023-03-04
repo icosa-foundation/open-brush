149f58c9f96d88d3798e35e1ad32e01ef8846c4d
Andy Baker <andy@andybak.net>
2023-03-04 14:00:29 +0000

    Should be ActionButton not OptionButton

--- Assets/Scripts/GUI/AppSettingsPanel.cs
+++ Assets/Scripts/GUI/AppSettingsPanel.cs
@@ -12,24 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
 using UnityEngine;
-using UnityEngine.Serialization;
-
 namespace TiltBrush
 {
 
     public class AppSettingsPanel : BasePanel
     {
-        public ToggleButton m_ToggleExperimentalModeToggle;
 
-        public override void InitPanel()
-        {
-            base.InitPanel();
-            m_ToggleExperimentalModeToggle.IsToggledOn = App.Config.GetIsExperimental();
-        }
-
-        public void HandleToggleHandedness(ToggleButton btn)
+        public void HandleToggleHandedness()
         {
             SketchControlsScript.DoSwapControls();
         }
