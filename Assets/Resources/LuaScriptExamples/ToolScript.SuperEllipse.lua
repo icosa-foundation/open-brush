f66d10766954d820c5c470e7fb9ebba26bbb6591
Andy Baker <andy@andybak.net>
2023-03-07 15:51:13 +0000

    Another script fix
    [CI BUILD]

--- Assets/Resources/LuaScriptExamples/ToolScript.SuperEllipse.lua
+++ Assets/Resources/LuaScriptExamples/ToolScript.SuperEllipse.lua
@@ -1,6 +1,4 @@
-﻿Settings = {
-    previewType="quad"
-}
+﻿Settings = {previewType="quad"}
 
 Widgets = {
     n={label="n", type="float", min=0.01, max=5, default=3.5},
@@ -14,11 +12,11 @@ function OnTriggerReleased()
     points = {}
     for i = 0, 360, 10 do
         angle = i * math.pi / 180
-        x = math.pow(math.abs(math.cos(angle)), 2/ distance) * sign(math.cos(angle))
-        y = math.pow(math.abs(math.sin(angle)), 2/ distance) * sign(math.sin(angle))
+        x = math.pow(math.abs(math.cos(angle)), 2/n) * sign(math.cos(angle))
+        y = math.pow(math.abs(math.sin(angle)), 2/n) * sign(math.sin(angle))
         position = { x, y, 0}
         rotation = { 0, 0, angle * 180}
-        table.insert(points, { position, rotation })
+        table.insert(points, {position, rotation})
     end
     return points
 end
