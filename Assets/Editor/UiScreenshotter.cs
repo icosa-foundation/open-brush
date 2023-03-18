f89c07a08b6e996d9ac83f6da31aff4f7429c074
Andy Baker <andy@andybak.net>
2023-03-18 16:22:35 +0000

    Automatic screenshots of panels (mainly for docs)

---
+++ Assets/Editor/UiScreenshotter.cs
@@ -0,0 +1,93 @@
+﻿// Copyright 2023 The Tilt Brush Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+namespace TiltBrush
+{
+    public class UiScreenshotter : Editor
+    {
+
+        static public TrTransform panelTr;
+
+        [MenuItem("Open Brush/Info/Generate UI Screenshots")]
+        static void Generate()
+        {
+            if (!Application.isPlaying)
+            {
+                Debug.LogError("You can only run this whilst in Play Mode");
+                return;
+            }
+
+            var blackGuid = Guid.Parse("580b4529-ac50-4fe9-b8d2-635765a14893");
+            var env = EnvironmentCatalog.m_Instance.GetEnvironment(blackGuid);
+            SceneSettings.m_Instance.SetDesiredPreset(env,
+                keepSceneTransform: true, forceTransition: false, hasCustomLights: false, skipFade: true);
+
+            foreach (BasePanel.PanelType panelType in (BasePanel.PanelType[])Enum.GetValues(typeof(BasePanel.PanelType)))
+            {
+                if (!PanelManager.m_Instance.IsPanelOpen(panelType))
+                {
+                    PanelManager.m_Instance.OpenPanel(panelType, TrTransform.T(new Vector3(0, 50, 2)));
+                }
+            }
+            DelayedTasks();
+        }
+
+        async static void DelayedTasks()
+        {
+            await Task.Delay(3000);
+
+            var cam = Camera.main;
+            cam.transform.position = new Vector3(0, 100, 0);
+            cam.transform.rotation = Quaternion.identity;
+
+            foreach (BasePanel.PanelType panelType in (BasePanel.PanelType[]) Enum.GetValues(typeof(BasePanel.PanelType)))
+            {
+                TrTransform panelTr = TrTransform.T(new Vector3(-1, 100, 4));
+                if (PanelManager.m_Instance.IsPanelOpen(panelType))
+                {
+                    BasePanel panel = PanelManager.m_Instance.GetPanelByType(panelType);
+                    panel.PanelGazeActive(true);
+                    await Task.Delay(500);
+                    var originalTransform = TrTransform.FromTransform(panel.transform);
+                    panelTr.ToTransform(panel.transform);
+                    panel.ResetReticleOffset();
+                    SaveCurrentView(cam, $"panel-{panelType}.png", 1280, 1280);
+                    originalTransform.ToTransform(panel.transform);
+                }
+            }
+        }
+
+        static void SaveCurrentView(Camera cameraToCapture, string fileName, int resWidth, int resHeight)
+        {
+            RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+            cameraToCapture.targetTexture = rt;
+            Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+            cameraToCapture.Render();
+            RenderTexture.active = rt;
+            screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+            cameraToCapture.targetTexture = null;
+            RenderTexture.active = null;
+            Destroy(rt);
+            byte[] bytes = screenShot.EncodeToPNG();
+            string filePath = Path.Combine(System.IO.Directory.GetCurrentDirectory(), fileName);
+            File.WriteAllBytes(filePath, bytes);
+        }
+    }
+}
