84c1c07119cc4efbc25f1c0c4490d208a5621672
Andy Baker <andy@andybak.net>
2023-04-27 09:32:00 +0100

    Fix copyright messages

--- Assets/Scripts/GUI/KeyboardPopUpWindow.cs
+++ Assets/Scripts/GUI/KeyboardPopUpWindow.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2022 The Tilt Brush Authors
+﻿// Copyright 2023 The Open Brush Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
