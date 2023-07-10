1bfcd0a192326b7923298d2d7d76e532e9c45f2e
Andy Baker <andy@andybak.net>
2023-07-10 09:59:14 +0100

    Switch IsoMesh to UPM

--- Assets/ThirdParty/IsoMesh/Utilities/ReadOnlyAttribute.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-/// <summary>
-/// Read Only attribute.
-/// Attribute is use only to mark ReadOnly properties.
-/// </summary>
-public class ReadOnlyAttribute : PropertyAttribute { }
