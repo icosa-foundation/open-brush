4ab6a3e52c4a25146ec5f10f28a364d2b14a2f7e
Andy Baker <andy@andybak.net>
2023-07-23 11:20:50 +0100

    Simplify transform code

--- Assets/Scripts/API/Lua/Wrappers/CameraPathApiWrapper.cs
+++ Assets/Scripts/API/Lua/Wrappers/CameraPathApiWrapper.cs
@@ -93,42 +93,21 @@ namespace TiltBrush
         public Vector3 position
         {
             get => transform.translation;
-            set
-            {
-                var tr_CS = transform;
-                var newTransform = TrTransform.T(value);
-                newTransform = App.Scene.Pose * newTransform;
-                tr_CS.translation = newTransform.translation;
-                transform = tr_CS;
-            }
+            set => transform = TrTransform.TRS(value, transform.rotation, transform.scale);
         }
 
         [LuaDocsDescription("The 3D orientation of the Brush Camera Path")]
         public Quaternion rotation
         {
             get => transform.rotation;
-            set
-            {
-                var tr_CS = transform;
-                var newTransform = TrTransform.R(value);
-                newTransform = App.Scene.Pose * newTransform;
-                tr_CS.rotation = newTransform.rotation;
-                transform = tr_CS;
-            }
+            set => transform = TrTransform.TRS(transform.translation, value, transform.scale);
         }
 
         [LuaDocsDescription("The scale of the camera path")]
         public float scale
         {
             get => transform.scale;
-            set
-            {
-                var tr_CS = transform;
-                var newTransform = TrTransform.S(value);
-                newTransform = App.Scene.Pose * newTransform;
-                tr_CS.scale = newTransform.scale;
-                transform = tr_CS;
-            }
+            set => transform = TrTransform.TRS(transform.translation, transform.rotation, value);
         }
 
         [LuaDocsDescription("Renders the currently active path")]
--- Assets/Scripts/API/Lua/Wrappers/ModelApiWrapper.cs
+++ Assets/Scripts/API/Lua/Wrappers/ModelApiWrapper.cs
@@ -51,42 +51,21 @@ namespace TiltBrush
         public Vector3 position
         {
             get => transform.translation;
-            set
-            {
-                var tr_CS = transform;
-                var newTransform = TrTransform.T(value);
-                newTransform = App.Scene.Pose * newTransform;
-                tr_CS.translation = newTransform.translation;
-                transform = tr_CS;
-            }
+            set => transform = TrTransform.TRS(value, transform.rotation, transform.scale);
         }
 
         [LuaDocsDescription(@"The 3D orientation of the Model Widget")]
         public Quaternion rotation
         {
             get => transform.rotation;
-            set
-            {
-                var tr_CS = transform;
-                var newTransform = TrTransform.R(value);
-                newTransform = App.Scene.Pose * newTransform;
-                tr_CS.rotation = newTransform.rotation;
-                transform = tr_CS;
-            }
+            set => transform = TrTransform.TRS(transform.translation, value, transform.scale);
         }
 
         [LuaDocsDescription(@"The scale of the Model Widget")]
         public float scale
         {
             get => transform.scale;
-            set
-            {
-                var tr_CS = transform;
-                var newTransform = TrTransform.S(value);
-                newTransform = App.Scene.Pose * newTransform;
-                tr_CS.scale = newTransform.scale;
-                transform = tr_CS;
-            }
+            set => transform = TrTransform.TRS(transform.translation, transform.rotation, value);
         }
 
         [LuaDocsDescription(@"Imports a new model from the MediaLibrary/Models folder")]
