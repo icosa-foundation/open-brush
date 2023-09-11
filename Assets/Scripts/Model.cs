597013c2500b9d9d712a92d4579fbc1330778ffa
Andy Baker <andy@andybak.net>
2023-09-11 02:19:26 +0100

    Fixes 3D models being omitted from GLTF export since switch to GLTFast (#526)
    * Generate a valid list of exportable materials and set m_AllowExport to true
    
    * dotnet format
    
    * Remove unused parameter

--- Assets/Scripts/Model.cs
+++ Assets/Scripts/Model.cs
@@ -512,12 +512,13 @@ namespace TiltBrush
                     this,
                     warnings
                 );
-
+                m_AllowExport = true;
                 await t;
 
             }
             catch (Exception ex)
             {
+                m_AllowExport = false;
                 m_LoadError = new LoadError("Invalid data", ex.Message);
                 Debug.LogException(ex);
             }
@@ -891,5 +892,16 @@ namespace TiltBrush
             return "Unknown";
         }
 
+        public void AssignMaterialsToCollector(ImportMaterialCollector collector)
+        {
+            m_ImportMaterialCollector = collector;
+            foreach (var mf in GetMeshes())
+            {
+                foreach (var unityMat in mf.GetComponent<MeshRenderer>().materials)
+                {
+                    m_ImportMaterialCollector.Add(unityMat);
+                }
+            }
+        }
     }
 } // namespace TiltBrush;
