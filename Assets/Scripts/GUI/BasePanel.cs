1467d29a2bb7b2650646bfa848588cabb6a3dd05
Andy Baker <andy@andybak.net>
2023-09-30 11:05:42 +0100

    Small refactor to allow directly opening popups from code (WIP)

--- Assets/Scripts/GUI/BasePanel.cs
+++ Assets/Scripts/GUI/BasePanel.cs
@@ -1486,45 +1486,59 @@ namespace TiltBrush
 
                 if (iPopUpIndex >= 0)
                 {
-                    // Create a new popup.
-                    GameObject popUp = (GameObject)Instantiate(m_PanelPopUpMap[iPopUpIndex].m_PopUpPrefab,
-                        m_Mesh.transform.position, m_Mesh.transform.rotation);
-                    m_ActivePopUp = popUp.GetComponent<PopUpWindow>();
-
+                    Vector3 position;
                     // If we're replacing a popup, put the new one in the same position.
                     if (bPopUpExisted)
                     {
-                        popUp.transform.position = vPrevPopUpPos;
+                        position = vPrevPopUpPos;
                     }
                     else
                     {
                         Vector3 vPos = m_Mesh.transform.position +
                             (m_Mesh.transform.forward * m_ActivePopUp.GetPopUpForwardOffset()) +
                             m_Mesh.transform.TransformVector(vPopupOffset);
-                        popUp.transform.position = vPos;
+                        position = vPos;
                     }
-                    popUp.transform.parent = m_Mesh.transform;
-                    m_ActivePopUp.Init(gameObject, sDelayedText);
-                    m_ActivePopUp.SetPopupCommandParameters(iCommandParam, iCommandParam2);
-
-                    m_ActivePopUp.m_OnClose += delayedClose;
-                    m_PopUpGazeTimer = 0;
-                    m_EatInput = !m_ActivePopUp.IsLongPressPopUp();
-
-                    // If we closed a popup to create this one, we wan't don't want the standard visual
-                    // fade that happens when a popup comes in.  We're spoofing the transition value
-                    // for visuals to avoid a pop.
-                    if (bPopUpExisted)
-                    {
-                        m_ActivePopUp.SpoofTransitionValue();
-                    }
-
-                    // Cache the intended command.
-                    m_DelayedCommand = rCommand;
-                    m_DelayedCommandParam = iCommandParam;
-                    m_DelayedCommandParam2 = iCommandParam2;
+                    CreatePopUp(m_PanelPopUpMap[iPopUpIndex].m_PopUpPrefab, position, bPopUpExisted, iCommandParam,
+                        iCommandParam2, rCommand, sDelayedText, delayedClose);
                 }
             }
+        }
+
+        public void CreatePopUp(
+                            GameObject prefab, Vector3 position,
+                            bool transition = true, int iCommandParam = -1, int iCommandParam2 = -1,
+                            SketchControlsScript.GlobalCommands delayedCommand = SketchControlsScript.GlobalCommands.Null,
+                            string sDelayedText = "", Action delayedClose = null)
+        {
+            // Create a new popup.
+            GameObject popUp = Instantiate(prefab,
+                m_Mesh.transform.position, m_Mesh.transform.rotation);
+            m_ActivePopUp = popUp.GetComponent<PopUpWindow>();
+
+            popUp.transform.position = position;
+
+            popUp.transform.parent = m_Mesh.transform;
+            m_ActivePopUp.Init(gameObject, sDelayedText);
+            m_ActivePopUp.SetPopupCommandParameters(iCommandParam, iCommandParam2);
+
+            m_ActivePopUp.m_OnClose += delayedClose;
+            m_PopUpGazeTimer = 0;
+            m_EatInput = !m_ActivePopUp.IsLongPressPopUp();
+
+            // If we closed a popup to create this one, we wan't don't want the standard visual
+            // fade that happens when a popup comes in.  We're spoofing the transition value
+            // for visuals to avoid a pop.
+            if (!transition)
+            {
+                m_ActivePopUp.SpoofTransitionValue();
+            }
+
+            // Cache the intended command.
+            m_DelayedCommand = delayedCommand;
+            m_DelayedCommandParam = iCommandParam;
+            m_DelayedCommandParam2 = iCommandParam2;
+
         }
 
         public void PositionPopUp(Vector3 basePos)
