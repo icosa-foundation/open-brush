ff51afc6a7907a4264d3b3bf2552661df860fb67
Mike Nisbet <nisbet.mr@gmail.com>
2023-12-04 22:57:19 +0000

    Remove oculus specific changes

--- Assets/Scripts/PassthroughManager.cs
+++ Assets/Scripts/PassthroughManager.cs
@@ -4,18 +4,12 @@ namespace TiltBrush
 {
     public class PassthroughManager : MonoBehaviour
     {
-        public bool isHost;
-#if OCULUS_SUPPORTED
-        private OVRPassthroughLayer ovrPassthrough;
-#endif // OCULUS_SUPPORTED 
-
         void Start()
         {
 #if OCULUS_SUPPORTED
-            ovrPassthrough = gameObject.AddComponent<OVRPassthroughLayer>();
-            ovrPassthrough.overlayType = OVROverlay.OverlayType.Underlay;
-#endif // OCULUS_SUPPORTED  
-            OculusMRController.m_Instance.StartMRExperience(isHost);
+            var passthrough  = gameObject.AddComponent<OVRPassthroughLayer>();
+            passthrough.overlayType = OVROverlay.OverlayType.Underlay;
+#endif // OCULUS_SUPPORTED
         }
     }
 }
