5d8efd20969f8c8e5f8ce22a8f3914786ba58b7e
Andy Baker <andy@andybak.net>
2023-12-18 11:03:55 +0000

    Remove another check for the latk flag

--- Assets/Scripts/Export/ExportLatk.cs
+++ Assets/Scripts/Export/ExportLatk.cs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#if LATK_SUPPORTED
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
@@ -245,4 +244,3 @@ namespace TiltBrush
     }
 
 }
-#endif
