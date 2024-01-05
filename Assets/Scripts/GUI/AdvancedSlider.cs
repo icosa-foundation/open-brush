9724c6fad4a9566835766a047348d66cc19a6193
Andy Baker <andy@andybak.net>
2024-01-05 19:11:13 +0000

    Round slider values in the widget

--- Assets/Scripts/GUI/AdvancedSlider.cs
+++ Assets/Scripts/GUI/AdvancedSlider.cs
@@ -163,6 +163,10 @@ namespace TiltBrush
         private void _UpdateValueAbsolute(float fValue)
         {
             valueText.text = FormatValue(fValue);
+            if (SliderType == SliderTypes.Int)
+            {
+                fValue = Mathf.RoundToInt(fValue);
+            }
             onUpdateValue.Invoke(new Vector3(m_Param1, m_Param2, fValue));
             m_CurrentValue = Mathf.InverseLerp(Min, Max, fValue);
             SetSliderPositionToReflectValue();
