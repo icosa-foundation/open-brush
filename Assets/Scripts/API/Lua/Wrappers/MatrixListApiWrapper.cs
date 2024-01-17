de1231fd8962870b35617e6b76399a50c502395b
Andy Baker <andy@andybak.net>
2024-01-17 15:24:44 +0000

    MatrixListWrapper should implement IPathApiWrapper

--- Assets/Scripts/API/Lua/Wrappers/MatrixListApiWrapper.cs
+++ Assets/Scripts/API/Lua/Wrappers/MatrixListApiWrapper.cs
@@ -7,10 +7,19 @@ namespace TiltBrush
 {
     [LuaDocsDescription("A list of transformation matrices")]
     [MoonSharpUserData]
-    public class MatrixListApiWrapper
+    public class MatrixListApiWrapper : IPathApiWrapper
     {
         [MoonSharpHidden]
         public List<Matrix4x4> _Matrices;
+
+        [MoonSharpHidden]
+        public ScriptCoordSpace _Space { get; set; }
+
+        [MoonSharpHidden]
+        public List<TrTransform> AsSingleTrList() => _Matrices.Select(TrTransform.FromMatrix4x4).ToList();
+
+        [MoonSharpHidden]
+        public List<List<TrTransform>> AsMultiTrList() => new(){ AsSingleTrList() };
 
         public MatrixListApiWrapper()
         {
@@ -32,6 +41,5 @@ namespace TiltBrush
 
         [LuaDocsDescription("The number of matrices")]
         public int count => _Matrices?.Count ?? 0;
-
     }
 }
