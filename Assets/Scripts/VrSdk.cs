f061e1aceee05e4e0b1b30b650605a7c2a0f0857
Andy Baker <andy@andybak.net>
2024-05-16 15:13:17 +0100

    See if this affects the passthrough transparancy issue
    [CI BUILD]

--- Assets/Scripts/VrSdk.cs
+++ Assets/Scripts/VrSdk.cs
@@ -230,9 +230,6 @@ namespace TiltBrush
                     var unused = msg.Data.AgeCategory;
                 });
             }
-#if PASSTHROUGH_SUPPORTED
-            OVRManager.eyeFovPremultipliedAlphaModeEnabled = false;
-#endif
 
 #endif // OCULUS_SUPPORTED
 
