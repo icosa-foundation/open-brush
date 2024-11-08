3445c02c5b8f1ea2df6dde6c72b3cef28aeaee0e
Riccardo Bovo <rb1619@ic.ac.uk>
2024-11-08 20:12:13 +0000

    Update MultiplayerManagerEditor.cs
     Enhanced MultiplayerManager Editor to streamline debugging with clearer information display and controls.

--- Assets/Editor/MultiplayerManagerEditor.cs
+++ Assets/Editor/MultiplayerManagerEditor.cs
@@ -3,6 +3,7 @@ using UnityEditor;
 using UnityEngine;
 using OpenBrush.Multiplayer;
 using System.Threading.Tasks;
+using System.ComponentModel.Composition;
 
 #if UNITY_EDITOR
 [CustomEditor(typeof(MultiplayerManager))]
@@ -16,65 +17,99 @@ public class MultiplayerManagerInspector : Editor
 
     public override void OnInspectorGUI()
     {
-        // Get the target object (MultiplayerManager)
         multiplayerManager = (MultiplayerManager)target;
 
-        GUILayout.Label("Multiplayer Manager Controls", EditorStyles.boldLabel);
+        DrawDefaultInspector();
 
-        // Room data input fields
+        GUILayout.Space(10);
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        GUILayout.Space(10);
+
         roomName = EditorGUILayout.TextField("Room Name", roomName);
 
-        // Display m_LocalPlayer if assigned
-        if (multiplayerManager.m_LocalPlayer != null)
-        {
-            EditorGUILayout.LabelField("Local Player Data", EditorStyles.boldLabel);
-            EditorGUILayout.LabelField("UserId", multiplayerManager.m_LocalPlayer.PlayerId.ToString());
-        }
-        else
-        {
-            EditorGUILayout.LabelField("Local Player is not assigned.");
-        }
+        //State
+        string connectionState = "";
+        if (multiplayerManager != null) connectionState = multiplayerManager.State.ToString();
+        else connectionState = "Not Assigned";
 
-        // Display each entry in m_RemotePlayers list
-        if (multiplayerManager.m_RemotePlayers != null && multiplayerManager.m_RemotePlayers.Count > 0)
-        {
-            EditorGUILayout.LabelField("Remote Players Data", EditorStyles.boldLabel);
-            foreach (var remotePlayer in multiplayerManager.m_RemotePlayers)
-            {
-                EditorGUILayout.LabelField("UserId", remotePlayer.PlayerId.ToString());
-            }
-        }
-        else
-        {
-            EditorGUILayout.LabelField("Remote Players are not assigned.");
-        }
+        GUILayout.Space(10);
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Connection State: ", EditorStyles.boldLabel);
+        EditorGUILayout.LabelField($"{connectionState}");
+        EditorGUILayout.EndHorizontal();
+        GUILayout.Space(5);
 
-        // Button to join the lobby
-        if (GUILayout.Button("Join Lobby") )
+        if (GUILayout.Button("Connect") )
         {
             ConnectToLobby();
-            EditorUtility.SetDirty(target); // Mark the object as dirty to recognize state changes
+            EditorUtility.SetDirty(target); 
         }
 
-        // Button to join the room
+
         if (GUILayout.Button("Join Room"))
         {
             ConnectToRoom();
-            EditorUtility.SetDirty(target); // Update inspector on state change
+            EditorUtility.SetDirty(target); 
         }
 
-        // Button to exit the room
+ 
         if (GUILayout.Button("Exit Room") )
         {
             DisconnectFromRoom();
-            EditorUtility.SetDirty(target); // Update inspector on state change
+            EditorUtility.SetDirty(target); 
         }
 
-        // Force the inspector to repaint to reflect the latest state
-        Repaint();
 
-        // Draw default inspector below
-        DrawDefaultInspector();
+        if (GUILayout.Button("Disconnect"))
+        {
+            Disconnect();
+            EditorUtility.SetDirty(target); 
+        }
+
+
+        //Local Player Id
+        string localPlayerId = "";
+        if (multiplayerManager.m_LocalPlayer != null) localPlayerId = multiplayerManager.m_LocalPlayer.PlayerId.ToString();
+        else localPlayerId = "Not Assigned";
+
+        GUILayout.Space(10);
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Local Player ID: ", EditorStyles.boldLabel);
+        EditorGUILayout.LabelField($"{localPlayerId}");
+        EditorGUILayout.EndHorizontal();
+
+        //Room Ownership
+        string ownership = "";
+        if (multiplayerManager != null && multiplayerManager.isUserRoomOwner) ownership = "Yes";
+        else if (multiplayerManager != null && !multiplayerManager.isUserRoomOwner) ownership = "No";
+        else ownership = "Not Assigned";
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Is Local Player Room Owner:", EditorStyles.boldLabel);
+        EditorGUILayout.LabelField($"{ownership}");
+        EditorGUILayout.EndHorizontal();
+
+        //Remote Users
+        string remoteUsersRegistered = "";
+        if (multiplayerManager.m_RemotePlayers != null && multiplayerManager.m_RemotePlayers.Count > 0)
+        {
+            remoteUsersRegistered = "UserIds:[ ";
+            foreach (var remotePlayer in multiplayerManager.m_RemotePlayers)
+                remoteUsersRegistered += remotePlayer.PlayerId.ToString()+",";
+            remoteUsersRegistered += "]";
+        }
+        else remoteUsersRegistered = "Not Assigned";
+
+        //Registered remote players
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Registered Remote Players IDs:", EditorStyles.boldLabel);
+        EditorGUILayout.LabelField($"{remoteUsersRegistered}");
+        EditorGUILayout.EndHorizontal();
+
+        Repaint();
+        
     }
 
     private async void ConnectToLobby()
@@ -108,6 +143,15 @@ public class MultiplayerManagerInspector : Editor
         {
             bool success = await multiplayerManager.LeaveRoom();
           
+        }
+    }
+
+    private async void Disconnect()
+    {
+        if (multiplayerManager != null)
+        {
+            bool success = await multiplayerManager.Disconnect();
+
         }
     }
 }
