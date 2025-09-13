17b6782fec9c6b3494819211411c95cab2229d73
Andy Baker <andy@andybak.net>
2025-09-13 01:21:03 +0100

    Fix animations not working due to node renaming for uniqueness (#904)
    * Fix animations not working due to node renaming for uniqueness
    
    * Unused import
    
    * Catch a potential null
    
    * dotnet-format
    
    * Update UnityGLTFSettings.asset
    
    * dummy commit
    
    * Use a heuristic to choose which clip to play
    
    * dotnet-format

--- Assets/Scripts/ImportGltfast.cs
+++ Assets/Scripts/ImportGltfast.cs
@@ -14,6 +14,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using TiltBrushToolkit;
 using UnityEngine;
@@ -75,7 +76,7 @@ namespace TiltBrush
                 ImportOptions options = new ImportOptions();
                 // TODO - should we import disabled to help round-tripping?
                 options.CameraImport = CameraImportOption.None;
-                options.AnimationMethod = AnimationMethod.Legacy;
+                options.AnimationMethod = AnimationMethod.Mecanim;
 
                 var normalizedPath = Uri.UnescapeDataString(localPath).Replace("\\", "/");
                 if (normalizedPath.StartsWith("/"))
@@ -90,6 +91,15 @@ namespace TiltBrush
                 gltf.IsMultithreaded = false;
                 AsyncHelpers.RunSync(() => gltf.LoadSceneAsync());
                 GameObject go = gltf.CreatedObject;
+
+                var clips = gltf.CreatedAnimationClips;
+                if (clips != null && clips.Length > 0)
+                {
+                    var player = go.AddComponent<PlayGltfAnimationClip>();
+                    // TODO - allow users to control autoplay
+                    player.PlayAnimation(clips);
+                }
+
                 model.CalcBoundsGltf(go);
                 model.EndCreatePrefab(go, warnings);
                 var materialCollector = new ImportMaterialCollector(assetLocation, uniqueSeed: localPath);
