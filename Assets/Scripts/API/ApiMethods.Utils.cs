5ed26cf2c8d1f5162ae913b8579130b587c9eef6
Andy Baker <andy@andybak.net>
2025-09-27 12:08:16 +0100

    Remove blocking of api scale changes

--- Assets/Scripts/API/ApiMethods.Utils.cs
+++ Assets/Scripts/API/ApiMethods.Utils.cs
@@ -156,11 +156,6 @@ namespace TiltBrush
 
         private static void _SetWidgetScale(GrabWidget widget, float scale)
         {
-            // Don't override size for ImageWidgets that have preserve flag set
-            if (widget is ImageWidget imageWidget && imageWidget.ShouldPreserveCustomSizePublic())
-            {
-                return;
-            }
             widget.SetSignedWidgetSize(scale);
         }
 
