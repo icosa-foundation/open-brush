669cab40ee7b36f5fa35dacb8d11b04bd9d2f4d5
Andy Baker <andy@andybak.net>
2025-09-29 12:02:57 +0100

    Close the loop on paths that have gaps

--- Assets/Resources/LuaScriptExamples/ToolScript.SuperEllipse.lua
+++ Assets/Resources/LuaScriptExamples/ToolScript.SuperEllipse.lua
@@ -12,12 +12,13 @@ function Main()
         points = Path:New()
         for i = 0, 360, 10 do
             angle = i * Math.pi / 180
-            x = Math:Pow(Math:Abs(Math:Cos(angle)), 2/Parameters.n) * Math:Sign(Math:Cos(angle))
-            y = Math:Pow(Math:Abs(Math:Sin(angle)), 2/Parameters.n) * Math:Sign(Math:Sin(angle))
+            x = Math:Pow(Math:Abs(Math:Cos(angle)), 2 / Parameters.n) * Math:Sign(Math:Cos(angle))
+            y = Math:Pow(Math:Abs(Math:Sin(angle)), 2 / Parameters.n) * Math:Sign(Math:Sin(angle))
             position = Vector3:New(x, y, 0)
             rotation = Rotation:New(0, 0, angle * 180)
             points:Insert(Transform:New(position, rotation))
         end
+        points:Insert(points[0]) -- Close the loop
         return points
     end
 end
