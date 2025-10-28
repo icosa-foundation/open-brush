d28b9ab5d8d14a1b537e8ce7f8b17b62f7c9d850
Andy Baker <andy@andybak.net>
2025-10-28 16:26:48 +0000

    export filenames should match the same rules that Django uses (#936)
    * export filenames should match the same rules that Django uses

--- Assets/Scripts/Sharing/VrAssetService.cs
+++ Assets/Scripts/Sharing/VrAssetService.cs
@@ -677,6 +677,11 @@ namespace TiltBrush
 
             var currentScene = SaveLoadScript.m_Instance.SceneFile;
             string uploadName = currentScene.Valid ? currentScene.HumanName : kDefaultName;
+            uploadName = FileUtils.GetValidFilename(uploadName);
+            if (string.IsNullOrEmpty(uploadName))
+            {
+                uploadName = FileUtils.GetValidFilename(kDefaultName);
+            }
             string gltfUploadName = $"{uploadName}.gltf";
 
             SetUploadProgress(UploadStep.CreateGltf, 0);
