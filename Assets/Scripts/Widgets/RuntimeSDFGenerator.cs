04dca4fca04ea5229f3bca917f423200407a0448
Andy Baker <andy@andybak.net>
2025-11-21 10:01:18 +0000

    tidy up

--- Assets/Scripts/Widgets/RuntimeSDFGenerator.cs
+++ Assets/Scripts/Widgets/RuntimeSDFGenerator.cs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
-using System.Collections;
 using UnityEngine;
 using IsoMesh;
 
