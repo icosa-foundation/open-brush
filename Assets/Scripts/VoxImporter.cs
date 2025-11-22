99b43e0a6fee4ec3e10b5229988830d7b2dc7f8a
Andy Baker <andy@andybak.net>
2025-11-22 16:14:22 +0000

    add a credit to the copyright message

--- Assets/Scripts/VoxImporter.cs
+++ Assets/Scripts/VoxImporter.cs
@@ -11,6 +11,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// Parts are based on:
+// https://github.com/darkfall/MagicaVoxelUnity
+// Copyright (c) 2015 Ruiwei Bu
 
 using System;
 using System.Collections.Generic;
