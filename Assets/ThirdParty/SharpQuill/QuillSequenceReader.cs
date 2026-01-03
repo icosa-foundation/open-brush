0c789f3b95b9d820edeae8ad173858fdad3727f2
Andy Baker <andy@andybak.net>
2026-01-03 15:49:16 +0000

    Fix IL2CPP issues

--- Assets/ThirdParty/SharpQuill/QuillSequenceReader.cs
+++ Assets/ThirdParty/SharpQuill/QuillSequenceReader.cs
@@ -30,7 +30,7 @@ namespace SharpQuill
       try
       {
         string json = File.ReadAllText(sequenceFilename);
-        dynamic document = JsonConvert.DeserializeObject(json);
+        JToken document = JsonConvert.DeserializeObject<JToken>(json);
         seq = Parse(document);
       }
       catch(JsonReaderException e)
@@ -58,45 +58,45 @@ namespace SharpQuill
       return seq;
     }
 
-    private static Sequence Parse(dynamic s)
+    private static Sequence Parse(JToken s)
     {
       Sequence seq = new Sequence();
-      if (s == null || s.Sequence == null)
+      if (s == null || s["Sequence"] == null)
         return seq;
 
-      seq.Metadata = ParseMetadata(s.Sequence.Metadata);
-      seq.Gallery = ParseGallery(s.Sequence.Gallery);
-      seq.BackgroundColor = ParseColor(s.Sequence.BackgroundColor);
-      if (s.Sequence.DefaultViewpoint != null)
-        seq.DefaultViewpoint = s.Sequence.DefaultViewpoint.ToObject(typeof(string));
+      seq.Metadata = ParseMetadata(s["Sequence"]["Metadata"]);
+      seq.Gallery = ParseGallery(s["Sequence"]["Gallery"]);
+      seq.BackgroundColor = ParseColor(s["Sequence"]["BackgroundColor"] as JArray);
+      if (s["Sequence"]["DefaultViewpoint"] != null)
+        seq.DefaultViewpoint = (string)s["Sequence"]["DefaultViewpoint"].ToObject(typeof(string));
       else
         seq.DefaultViewpoint = ""; // "Root/InitialSpawnArea"
 
-      seq.Framerate = ParseInt(s.Sequence.Framerate, seq.Framerate);
-      seq.ExportStart = ParseInt(s.Sequence.ExportStart, seq.ExportStart);
-      seq.ExportEnd = ParseInt(s.Sequence.ExportEnd, seq.ExportEnd);
-      seq.CameraResolution = ParseSize(s.Sequence.CameraResolution, seq.CameraResolution);
-      seq.RootLayer = ParseLayer(s.Sequence.RootLayer);
+      seq.Framerate = ParseInt(s["Sequence"]["Framerate"], seq.Framerate);
+      seq.ExportStart = ParseInt(s["Sequence"]["ExportStart"], seq.ExportStart);
+      seq.ExportEnd = ParseInt(s["Sequence"]["ExportEnd"], seq.ExportEnd);
+      seq.CameraResolution = ParseSize(s["Sequence"]["CameraResolution"] as JArray, seq.CameraResolution);
+      seq.RootLayer = ParseLayer(s["Sequence"]["RootLayer"]) as LayerGroup;
 
       seq.RootLayer.Animation.Timeline = true;
       return seq;
     }
 
-    private static Metadata ParseMetadata(dynamic m)
+    private static Metadata ParseMetadata(JToken m)
     {
       Metadata metadata = new Metadata();
       if (m == null)
         return metadata;
 
-      metadata.Title = m.Title.ToObject(typeof(string));
-      metadata.Description = m.Description.ToObject(typeof(string));
-      if (m.ThumbnailCropPosition != null)
-        metadata.ThumbnailCropPosition = m.ThumbnailCropPosition.ToObject(typeof(float));
-      
+      metadata.Title = (string)m["Title"].ToObject(typeof(string));
+      metadata.Description = (string)m["Description"].ToObject(typeof(string));
+      if (m["ThumbnailCropPosition"] != null)
+        metadata.ThumbnailCropPosition = (float)m["ThumbnailCropPosition"].ToObject(typeof(float));
+
       return metadata;
     }
 
-    private static Gallery ParseGallery(dynamic g)
+    private static Gallery ParseGallery(JToken g)
     {
       Gallery gallery = new Gallery();
       if (g == null)
@@ -110,19 +110,19 @@ namespace SharpQuill
       return gallery;
     }
 
-    private static int ParseInt(dynamic v, int def = 0)
+    private static int ParseInt(JToken v, int def = 0)
     {
-      return v ?? def;
+      return (v != null && v.Type != JTokenType.Null) ? v.ToObject<int>() : def;
     }
 
-    private static float ParseFloat(dynamic v, float def = 0)
+    private static float ParseFloat(JToken v, float def = 0)
     {
-      return v ?? def;
+      return (v != null && v.Type != JTokenType.Null) ? v.ToObject<float>() : def;
     }
 
-    private static bool ParseBool(dynamic v, bool def = false)
+    private static bool ParseBool(JToken v, bool def = false)
     {
-      return v ?? def;
+      return (v != null && v.Type != JTokenType.Null) ? v.ToObject<bool>() : def;
     }
 
     private static Color ParseColor(JArray jValue, Color def = new Color())
@@ -185,7 +185,7 @@ namespace SharpQuill
       return new Quaternion(value);
     }
 
-    private static Transform ParseTransform(dynamic t)
+    private static Transform ParseTransform(JToken t)
     {
       if (t == null)
         return Transform.Identity;
@@ -193,10 +193,10 @@ namespace SharpQuill
       if (t is JObject)
       {
         Transform transform = Transform.Identity;
-        transform.Rotation = ParseQuaternion(t.Rotation);
-        transform.Scale = ParseFloat(t.Scale, 1.0f);
-        transform.Flip = t.Flip;
-        transform.Translation = ParseVector3(t.Translation);
+        transform.Rotation = ParseQuaternion(t["Rotation"] as JArray);
+        transform.Scale = ParseFloat(t["Scale"], 1.0f);
+        transform.Flip = t["Flip"]?.ToObject<string>();
+        transform.Translation = ParseVector3(t["Translation"] as JArray);
         return transform;
       }
       else if (t is JArray)
@@ -228,51 +228,51 @@ namespace SharpQuill
     /// Parse the drawing metadata.
     /// The actual strokes will be read from the qbin later.
     /// </summary>
-    private static Drawing ParseDrawing(dynamic d)
+    private static Drawing ParseDrawing(JToken d)
     {
       Drawing drawing = new Drawing();
 
       if (d == null)
         return drawing;
 
-      drawing.BoundingBox = ParseBoundingBox(d.BoundingBox);
+      drawing.BoundingBox = ParseBoundingBox(d["BoundingBox"] as JArray);
 
       long offset;
-      bool parsed = long.TryParse((string)d.DataFileOffset.ToObject(typeof(string)), NumberStyles.HexNumber, CultureInfo.InvariantCulture, out offset);
+      bool parsed = long.TryParse((string)d["DataFileOffset"].ToObject(typeof(string)), NumberStyles.HexNumber, CultureInfo.InvariantCulture, out offset);
       drawing.DataFileOffset = parsed ? offset : -1;
 
       return drawing;
     }
     
-    private static KeepAlive ParseKeepAlive(dynamic ka)
+    private static KeepAlive ParseKeepAlive(JToken ka)
     {
       KeepAlive keepAlive = new KeepAlive();
       if (ka == null)
         return keepAlive;
 
-      keepAlive.Type = ParseEnum<KeepAliveType>(ka.Type);
+      keepAlive.Type = ParseEnum<KeepAliveType>(ka["Type"]);
       return keepAlive;
     }
 
-    private static Animation ParseAnimation(dynamic a)
+    private static Animation ParseAnimation(JToken a)
     {
       Animation animation = new Animation();
       if (a == null)
         return animation;
 
-      animation.Duration = ParseFloat(a.Duration);
-      animation.Timeline = ParseBool(a.Timeline);
-      animation.StartOffset = ParseFloat(a.StartOffset);
-      animation.MaxRepeatCount = ParseFloat(a.MaxRepeatCount);
-      animation.Keys = ParseKeyframes(a.Keys);
-      
+      animation.Duration = ParseFloat(a["Duration"]);
+      animation.Timeline = ParseBool(a["Timeline"]);
+      animation.StartOffset = ParseFloat(a["StartOffset"]);
+      animation.MaxRepeatCount = ParseFloat(a["MaxRepeatCount"]);
+      animation.Keys = ParseKeyframes(a["Keys"]);
+
       return animation;
     }
 
     /// <summary>
     /// Parse one enum. If not found this will return the default value.
     /// </summary>
-    private static T ParseEnum<T>(dynamic v) where T : struct
+    private static T ParseEnum<T>(JToken v) where T : struct
     {
       Enum.TryParse((string)v.ToObject(typeof(string)), out T result);
       return result;
@@ -281,60 +281,60 @@ namespace SharpQuill
     /// <summary>
     /// Parse all the keyframe channels data.
     /// </summary>
-    private static Keyframes ParseKeyframes(dynamic kkff)
+    private static Keyframes ParseKeyframes(JToken kkff)
     {
       Keyframes keyframes = new Keyframes();
       if (kkff == null)
         return keyframes;
 
-      if (kkff.Visibility != null)
+      if (kkff["Visibility"] != null)
       {
         keyframes.Visibility.Clear();
-        foreach (var kf in kkff.Visibility)
+        foreach (var kf in kkff["Visibility"])
         {
           Keyframe<bool> keyframe = new Keyframe<bool>();
-          keyframe.Time = kf.Time;
-          keyframe.Value = kf.Value;
-          keyframe.Interpolation = ParseEnum<Interpolation>(kf.Interpolation);
+          keyframe.Time = (int)kf["Time"].ToObject<float>();
+          keyframe.Value = kf["Value"].ToObject<bool>();
+          keyframe.Interpolation = ParseEnum<Interpolation>(kf["Interpolation"]);
           keyframes.Visibility.Add(keyframe);
         }
       }
 
-      if (kkff.Offset != null)
+      if (kkff["Offset"] != null)
       {
         keyframes.Offset.Clear();
-        foreach (var kf in kkff.Offset)
+        foreach (var kf in kkff["Offset"])
         {
           Keyframe<int> keyframe = new Keyframe<int>();
-          keyframe.Time = kf.Time;
-          keyframe.Value = kf.Value;
-          keyframe.Interpolation = ParseEnum<Interpolation>(kf.Interpolation);
+          keyframe.Time = (int)kf["Time"].ToObject<float>();
+          keyframe.Value = kf["Value"].ToObject<int>();
+          keyframe.Interpolation = ParseEnum<Interpolation>(kf["Interpolation"]);
           keyframes.Offset.Add(keyframe);
         }
       }
 
-      if (kkff.Opacity != null)
+      if (kkff["Opacity"] != null)
       {
         keyframes.Opacity.Clear();
-        foreach (var kf in kkff.Opacity)
+        foreach (var kf in kkff["Opacity"])
         {
           Keyframe<float> keyframe = new Keyframe<float>();
-          keyframe.Time = kf.Time;
-          keyframe.Value = kf.Value;
-          keyframe.Interpolation = ParseEnum<Interpolation>(kf.Interpolation);
+          keyframe.Time = (int)kf["Time"].ToObject<float>();
+          keyframe.Value = kf["Value"].ToObject<float>();
+          keyframe.Interpolation = ParseEnum<Interpolation>(kf["Interpolation"]);
           keyframes.Opacity.Add(keyframe);
         }
       }
 
-      if (kkff.Transform != null)
+      if (kkff["Transform"] != null)
       {
         keyframes.Transform.Clear();
-        foreach (var kf in kkff.Transform)
+        foreach (var kf in kkff["Transform"])
         {
           Keyframe<Transform> keyframe = new Keyframe<Transform>();
-          keyframe.Time = kf.Time;
-          keyframe.Value = ParseTransform(kf.Value);
-          keyframe.Interpolation = ParseEnum<Interpolation>(kf.Interpolation);
+          keyframe.Time = (int)kf["Time"].ToObject<float>();
+          keyframe.Value = ParseTransform(kf["Value"]);
+          keyframe.Interpolation = ParseEnum<Interpolation>(kf["Interpolation"]);
           keyframes.Transform.Add(keyframe);
         }
       }
@@ -345,20 +345,20 @@ namespace SharpQuill
     /// <summary>
     /// Parse one layer. Drill down recursively for groups.
     /// </summary>
-    private static Layer ParseLayer(dynamic l)
+    private static Layer ParseLayer(JToken l)
     {
       if (l == null)
         return null;
 
       Layer layer;
-      LayerType type = ParseEnum<LayerType>(l.Type);
+      LayerType type = ParseEnum<LayerType>(l["Type"]);
       switch (type)
       {
         case LayerType.Group:
           {
             layer = new LayerGroup();
             LayerGroup lg = layer as LayerGroup;
-            foreach (var c in l.Implementation.Children)
+            foreach (var c in l["Implementation"]["Children"])
             {
               Layer child = ParseLayer(c);
               if (child != null)
@@ -372,26 +372,26 @@ namespace SharpQuill
             layer = new LayerPaint();
             LayerPaint lp = layer as LayerPaint;
 
-            lp.Framerate = ParseInt(l.Implementation.Framerate, lp.Framerate);
-            lp.MaxRepeatCount = ParseInt(l.Implementation.MaxRepeatCount, lp.MaxRepeatCount);
+            lp.Framerate = ParseInt(l["Implementation"]["Framerate"], lp.Framerate);
+            lp.MaxRepeatCount = ParseInt(l["Implementation"]["MaxRepeatCount"], lp.MaxRepeatCount);
 
-            if (l.Implementation.Drawings != null && l.Implementation.Frames != null)
+            if (l["Implementation"]["Drawings"] != null && l["Implementation"]["Frames"] != null)
             {
-              foreach (var d in l.Implementation.Drawings)
+              foreach (var d in l["Implementation"]["Drawings"])
               {
                 Drawing drawing = ParseDrawing(d);
                 if (drawing != null)
                   lp.Drawings.Add(drawing);
               }
-              lp.Frames = l.Implementation.Frames.ToObject<List<int>>();
+              lp.Frames = l["Implementation"]["Frames"].ToObject<List<int>>();
             }
-            else if (l.Implementation.BoundingBox != null && l.Implementation.DataFileOffset != null)
+            else if (l["Implementation"]["BoundingBox"] != null && l["Implementation"]["DataFileOffset"] != null)
             {
               // Old format from Quill 1.3, circa 2017, before animations.
               Drawing drawing = new Drawing();
-              drawing.BoundingBox = ParseBoundingBox(l.Implementation.BoundingBox);
+              drawing.BoundingBox = ParseBoundingBox(l["Implementation"]["BoundingBox"] as JArray);
               long offset;
-              bool parsed = long.TryParse((string)l.Implementation.DataFileOffset.ToObject(typeof(string)), NumberStyles.HexNumber, CultureInfo.InvariantCulture, out offset);
+              bool parsed = long.TryParse((string)l["Implementation"]["DataFileOffset"].ToObject(typeof(string)), NumberStyles.HexNumber, CultureInfo.InvariantCulture, out offset);
               drawing.DataFileOffset = parsed ? offset : -1;
               lp.Drawings.Add(drawing);
               lp.Frames.Add(0);
@@ -402,22 +402,22 @@ namespace SharpQuill
           {
             layer = new LayerViewpoint();
             LayerViewpoint lv = layer as LayerViewpoint;
-            lv.Version = l.Implementation.Version;
-            lv.Color = ParseColor(l.Implementation.Color);
-            lv.Sphere = ParseVector4(l.Implementation.Sphere);
-            lv.AllowTranslationX = l.Implementation.AllowTranslationX;
-            lv.AllowTranslationY = l.Implementation.AllowTranslationY;
-            lv.AllowTranslationZ = l.Implementation.AllowTranslationZ;
-            lv.Exporting = l.Implementation.Exporting;
-            lv.ShowingVolume = l.Implementation.ShowingVolume;
-            lv.TypeStr = l.Implementation.TypeStr;
+            lv.Version = l["Implementation"]["Version"].ToObject<int>();
+            lv.Color = ParseColor(l["Implementation"]["Color"] as JArray);
+            lv.Sphere = ParseVector4(l["Implementation"]["Sphere"] as JArray);
+            lv.AllowTranslationX = l["Implementation"]["AllowTranslationX"].ToObject<bool>();
+            lv.AllowTranslationY = l["Implementation"]["AllowTranslationY"].ToObject<bool>();
+            lv.AllowTranslationZ = l["Implementation"]["AllowTranslationZ"].ToObject<bool>();
+            lv.Exporting = l["Implementation"]["Exporting"].ToObject<bool>();
+            lv.ShowingVolume = l["Implementation"]["ShowingVolume"].ToObject<bool>();
+            lv.TypeStr = l["Implementation"]["TypeStr"].ToObject<string>();
             break;
           }
         case LayerType.Camera:
           {
             layer = new LayerCamera();
             LayerCamera lc = layer as LayerCamera;
-            lc.FOV = l.Implementation.FOV;
+            lc.FOV = l["Implementation"]["FOV"].ToObject<float>();
             break;
           }
         case LayerType.Model:
@@ -438,19 +438,19 @@ namespace SharpQuill
     /// <summary>
     /// Parse the common part of the layer info.
     /// </summary>
-    private static void ParseLayerCommon(Layer layer, dynamic l)
+    private static void ParseLayerCommon(Layer layer, JToken l)
     {
-      layer.Name = l.Name;
-      layer.Visible = ParseBool(l.Visible, layer.Visible);
-      layer.Locked = ParseBool(l.Locked, layer.Locked);
-      layer.Collapsed = ParseBool(l.Collapsed, layer.Collapsed);
-      layer.BBoxVisible = ParseBool(l.BBoxVisible, layer.BBoxVisible);
-      layer.Opacity = ParseFloat(l.Opacity, layer.Opacity);
-      layer.IsModelTopLayer = ParseBool(l.IsModelTopLayer, layer.IsModelTopLayer);
-      layer.KeepAlive = ParseKeepAlive(l.KeepAlive);
-      layer.Transform = ParseTransform(l.Transform);
-      layer.Pivot = ParseTransform(l.Pivot);
-      layer.Animation = ParseAnimation(l.Animation);
+      layer.Name = l["Name"].ToObject<string>();
+      layer.Visible = ParseBool(l["Visible"], layer.Visible);
+      layer.Locked = ParseBool(l["Locked"], layer.Locked);
+      layer.Collapsed = ParseBool(l["Collapsed"], layer.Collapsed);
+      layer.BBoxVisible = ParseBool(l["BBoxVisible"], layer.BBoxVisible);
+      layer.Opacity = ParseFloat(l["Opacity"], layer.Opacity);
+      layer.IsModelTopLayer = ParseBool(l["IsModelTopLayer"], layer.IsModelTopLayer);
+      layer.KeepAlive = ParseKeepAlive(l["KeepAlive"]);
+      layer.Transform = ParseTransform(l["Transform"]);
+      layer.Pivot = ParseTransform(l["Pivot"]);
+      layer.Animation = ParseAnimation(l["Animation"]);
     }
 
     /// <summary>
