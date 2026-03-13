061b7c97375a7a3357d749f0adc90644ca66dcc7
Andy Baker <andy@andybak.net>
2026-03-13 13:47:23 +0000

    stripping tangents was a mistake based on a misunderstanding

--- Assets/Scripts/UnityGLTF Plugins/OpenBrushExportPlugin.cs
+++ Assets/Scripts/UnityGLTF Plugins/OpenBrushExportPlugin.cs
@@ -182,10 +182,6 @@ namespace TiltBrush
                         stroke.Recreate(null, canvas);
                         var mesh = stroke.m_Object.GetComponent<MeshFilter>().sharedMesh;
                         mesh = BrushBaker.m_Instance.ProcessMesh(mesh, stroke.m_BrushGuid.ToString());
-                        var strokeShaderName = BrushCatalog.m_Instance.GetBrush(stroke.m_BrushGuid)?.Material?.shader?.name;
-                        if (strokeShaderName != "Brush/StandardSingleSided" &&
-                            strokeShaderName != "Brush/StandardDoubleSided")
-                            mesh.tangents = null;
                         stroke.m_Object.GetComponent<MeshFilter>().sharedMesh = mesh;
                         stroke.m_Object.GetComponent<MeshFilter>().mesh = mesh;
                         stroke.m_Object.name = $"{stroke.m_Object.name}_{i}";
@@ -215,9 +211,6 @@ namespace TiltBrush
                     }
                     batch.m_EditorDebugMesh = mf.sharedMesh;
                     mesh = BrushBaker.m_Instance.ProcessMesh(mesh, brush.m_Guid.ToString());
-                    if (brush.Material.shader.name != "Brush/StandardSingleSided" &&
-                        brush.Material.shader.name != "Brush/StandardDoubleSided")
-                        mesh.tangents = null;
                     mf.sharedMesh = mesh;
                     mf.mesh = mesh;
                     // Swap the per-batch material instance back to the canonical brush material
