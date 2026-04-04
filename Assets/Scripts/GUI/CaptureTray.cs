237d204996edaf10ffda0437b169cf5e7afc04b6
Andy Baker <andy@andybak.net>
2026-04-04 15:12:11 +0100

    Revert Labs panel export buttons and use the new tray just for capture buttons

--- Assets/Scripts/GUI/CaptureTray.cs
+++ Assets/Scripts/GUI/CaptureTray.cs
@@ -12,13 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
-using UnityEngine;
-using UnityEngine.XR;
-
 namespace TiltBrush
 {
-    public class ExportTray : BaseTray
+    public class CaptureTray : BaseTray
     {
         public void ToggleTray(ActionToggleButton btn)
         {
