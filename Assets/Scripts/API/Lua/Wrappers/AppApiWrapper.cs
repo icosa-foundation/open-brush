0e82070294e24c10a84aaa528dbe64db027eebe9
Andy Baker <andy@andybak.net>
2026-08-02 13:01:40 +0100

    Validate API snapshot dimensions

--- Assets/Scripts/API/Lua/Wrappers/AppApiWrapper.cs
+++ Assets/Scripts/API/Lua/Wrappers/AppApiWrapper.cs
@@ -202,6 +202,7 @@ namespace TiltBrush
         public static void TakeSnapshot(TrTransform tr, string filename, int width, int height, float superSampling = 1f, bool renderDepth = false, bool removeBackground = false, bool renderNormals = false)
         {
             ApiMethods.ValidateSafeFilename(filename, "snapshot filename");
+            ApiMethods.ValidateSnapshotDimensions(width, height);
             ScreenshotManager.TakeSnapshot(tr, filename, width, height, superSampling, removeBackground, renderDepth, renderNormals);
         }
 
