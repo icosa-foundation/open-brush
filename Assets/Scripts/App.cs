dc90cf548db9644ccd5b7034d0cbd3e138128b9d
Andy Baker <andy@andybak.net>
2026-08-07 09:05:08 +0100

    Fix Gaussian capture output path

--- Assets/Scripts/App.cs
+++ Assets/Scripts/App.cs
@@ -2322,7 +2322,7 @@ namespace TiltBrush
 
         public static string SplatPosesPath()
         {
-            return Path.Combine(MediaLibraryPath(), "SplatPoses");
+            return Path.Combine(UserPath(), "SplatPoses");
         }
 
         static public string AutosavePath()
