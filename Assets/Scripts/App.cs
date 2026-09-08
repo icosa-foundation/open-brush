bc66e470413cd65c0aed57f272da4a120259a982
Andy Baker <andy@andybak.net>
2026-09-08 11:01:34 +0100

    Fix Quill and IMM library folder handling

--- Assets/Scripts/App.cs
+++ Assets/Scripts/App.cs
@@ -2291,23 +2291,13 @@ namespace TiltBrush
             }
         }
 
-        public static void InitQuillLibraryPath()
+        public static void InitQuillMediaLibraryPath()
         {
-            string quillLibraryDirectory = QuillLibraryPath();
+            string quillMediaDirectory = QuillMediaLibraryPath();
 
-            if (!Directory.Exists(quillLibraryDirectory))
+            if (!Directory.Exists(quillMediaDirectory))
             {
-                InitDirectoryAtPath(quillLibraryDirectory);
-            }
-        }
-
-        public static void InitQuillImmPath()
-        {
-            string quillImmDirectory = QuillImmPath();
-
-            if (!Directory.Exists(quillImmDirectory))
-            {
-                InitDirectoryAtPath(quillImmDirectory);
+                InitDirectoryAtPath(quillMediaDirectory);
             }
         }
 
@@ -2397,9 +2387,9 @@ namespace TiltBrush
                 System.Environment.SpecialFolder.Personal), "Quill");
         }
 
-        static public string QuillImmPath()
+        static public string QuillMediaLibraryPath()
         {
-            return Path.Combine(MediaLibraryPath(), "Imm");
+            return Path.Combine(MediaLibraryPath(), "Quill");
         }
 
         static public string AutosavePath()
