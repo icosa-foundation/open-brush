d286a9807abf71a43cf38e3ee730007426083ef3
Andy Baker <andy@andybak.net>
2026-09-10 15:46:14 +0100

    Preserve main's ZIP package migration with SAF stream support

--- Assets/Scripts/ZipSubfileReader.cs
+++ Assets/Scripts/ZipSubfileReader.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using ICSharpCode.SharpZipLib.Zip;
+using Unity.SharpZipLib.Zip;
 using System.IO;
 
 namespace TiltBrush
